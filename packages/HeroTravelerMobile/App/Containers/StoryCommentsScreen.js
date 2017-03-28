15951d3b99cbfc9ebcc220a8151bae867b2479be
Andrew Watt <awatt11215@gmail.com>
2017-03-28 18:19:48 -0400

    initial style

--- packages/HeroTravelerMobile/App/Containers/StoryCommentsScreen.js
+++ packages/HeroTravelerMobile/App/Containers/StoryCommentsScreen.js
@@ -1,17 +1,127 @@
 import React, { PropTypes } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, ScrollView, Image, TextInput, KeyboardAvoidingView } from 'react-native'
 import { connect } from 'react-redux'
+import RoundedButton from '../Components/RoundedButton'
+import { Colors, Metrics } from '../Themes/'
+import SearchBar from '../Components/SearchBar'
 
-import styles from './Styles/StoryReadingScreenStyles'
+import styles from './Styles/StoryCommentsScreenStyles'
+
+
+const Comment = ({avatar, name, comment, timestamp}) => {
+  return (
+    <View style={styles.commentWrapper}>
+      <View style={styles.comment}>
+        <Image style={styles.avatar} source={{uri: avatar }}></Image>
+        <View style={styles.commentTextWrapper}>
+          <Text style={styles.commentName}>{name}</Text>
+          <Text style={styles.commentText}>{comment}</Text>
+        </View>
+        <Text style={styles.timestamp}>{timestamp}</Text>
+      </View>
+    </View>
+  )
+}
+
+const List = ({children}) => {
+  return (
+    <ScrollView style={styles.list}>
+      {children}
+    </ScrollView>
+  )
+}
+
+
+const formatDate = function(stamp){
+  var duration = Date.now() - stamp;
+
+  if(duration < 86400000){
+    return 'Today';
+  } else if (duration < 172800000) {
+    return '1 day ago';
+  } else {
+    var newMonth = new Date(stamp).getMonth(),
+    newDate = new Date(stamp).getDate().toString(),
+    months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    return months[newMonth] + ' ' + newDate;
+  }
+}
 
 class StoryCommentsScreen extends React.Component {
+
+  static defaultProps = { 
+    comments: [{
+      comment: "This is my comment",
+      createdAt: 1490710993,
+      user: {
+        profile: {
+          fullName: "Vladimir Putin",
+          avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/solid_color/128.jpg"
+        }
+      }
+    }, {
+      comment: "This is another comment",
+      createdAt: 1490709965,
+      user: {
+        profile: {
+          fullName: "Andy Watt",
+          avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/solid_color/128.jpg"
+        }
+      }
+    }, {
+      comment: "This is another goshdarned comment",
+      createdAt: 1483228800000,
+      user: {
+        profile: {
+          fullName: "Jasper Johns",
+          avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/solid_color/128.jpg"
+        }
+      }
+    }, {
+      comment: "This is another goshdarned comment, Robin",
+      createdAt: 1490731854179,
+      user: {
+        profile: {
+          fullName: "The Batman",
+          avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/solid_color/128.jpg"
+        }
+      }
+    }]
+  }
+
+  static propTypes = {
+    comments: PropTypes.array,
+  }  
 
   render () {
 
     return (
-      <View style={[styles.containerWithNavbarAndTabbar, styles.root]}>
-        <Text>Comment UI goes here</Text>
-      </View>
+        <KeyboardAvoidingView behavior='position' style={[styles.containerWithNavbar, styles.root]}>
+          <List>
+          {this.props.comments.map(comment => {
+            return(
+              <Comment
+                avatar={comment.user.profile.avatar}
+                name={comment.user.profile.fullName}
+                comment={comment.comment}
+                timestamp={formatDate(comment.createdAt)}
+                key={comment.createdAt.toString()}
+              />
+              )})}          
+          </List>
+          <View style={styles.inputGroupWrapper}>
+            <View style={styles.inputWrapper}>
+                <TextInput
+                autoFocus
+                placeholder='Add a comment'
+                style={styles.input}
+                autoCapitalize='none'
+                autoCorrect={false}
+                />
+            </View>
+            <RoundedButton style={styles.inputButton}>Send</RoundedButton>
+          </View>
+        </KeyboardAvoidingView>
     )
   }
 }
---
+++ packages/HeroTravelerMobile/App/Containers/Styles/StoryCommentsScreenStyles.js
@@ -0,0 +1,91 @@
+import { StyleSheet } from 'react-native'
+import { ApplicationStyles, Colors, Metrics, Fonts } from '../../Themes/'
+
+export default StyleSheet.create({
+  ...ApplicationStyles.screen,
+  root: {
+    backgroundColor: Colors.snow,
+  },
+  list: {
+    height: Metrics.screenHeight - Metrics.navBarHeight - Metrics.tabBarHeight,
+    width: Metrics.screenWidth,
+  },
+  commentWrapper: {
+  	borderBottomWidth: 1,
+  	borderBottomColor: Colors.navBarText,
+  	backgroundColor: Colors.snow,
+  },
+  comment: {
+    padding: Metrics.doubleBaseMargin,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  commentTextWrapper: {
+    flexDirection: 'column',
+    width: '60%',
+    justifyContent: 'flex-start',
+  },  
+  commentText: {
+    fontFamily: Fonts.type.base,
+    fontSize: 16,
+    color: '#757575',
+    fontWeight: '300',
+    letterSpacing: .7,
+  },
+  commentName: {
+    fontSize: 16,
+    color: Colors.background,
+    fontWeight: '400',
+    letterSpacing: .7,
+  },
+  avatar: {
+    height: 36,
+    width: 36,
+    borderRadius: 18,
+    marginRight: 5,
+  },
+  timestamp: {
+    fontFamily: Fonts.type.base,
+    fontSize: 12,
+    color: '#757575',
+    fontWeight: '300',
+    letterSpacing: .7,
+    alignSelf: 'flex-start',
+  },
+  inputGroupWrapper: {
+  	flexDirection: 'row',
+  	height: Metrics.tabBarHeight,
+  	borderWidth: 1,
+  	borderColor: Colors.navBarText,	
+  	backgroundColor: Colors.lightGreyAreas,
+  	justifyContent: 'flex-start',
+  	alignItems: 'center',
+  },
+  inputWrapper: {
+  	width: '70%',
+  	height: Metrics.tabBarHeight/1.9,
+  	alignSelf: 'center',
+  	marginLeft: Metrics.doubleBaseMargin,
+  },
+  input: {
+	flex: 1,
+    backgroundColor: Colors.snow,
+    fontSize: Fonts.size.medium,
+    width: '100%',
+    textAlign: 'left',
+    fontFamily: Fonts.type.base,
+    fontSize: Fonts.size.input,
+    borderRadius: 6,
+    borderWidth: 1,
+    borderColor: Colors.navBarText,
+    color: '#757575',
+    paddingLeft: 4
+  },
+  inputButton: {
+  	width: '18%',
+  	height: Metrics.tabBarHeight/1.4,
+  	borderRadius: 14,
+  	marginLeft: '2%',
+  },
+})

3cc9a84553e78607c1745cc955db576724a63d1f
Andrew Watt <awatt11215@gmail.com>
2017-04-06 14:33:38 -0400

    update email logic

--- packages/ht-core/src/user/createUser.js
+++ packages/ht-core/src/user/createUser.js
@@ -1,24 +1,27 @@
 import joi from 'joi'
 import {User} from '../models'
 import encryptPassword from '../utils/encryptPassword'
-import sendEmail from '../utils/sendEmail'
+import sendEmail from '../utils/emailService/index'
 import getOrCreateTokens from './getOrCreateTokens'
 
 export default function createUser(userData) {
-  let userAttrs = Object.assign({}, userData)
+	let userAttrs = Object.assign({}, userData)
 
   // @TODO validate user
 
   return encryptPassword(userAttrs.password)
-    .then(hashedPassword => {
-	
-    //test email content
-	const msgSubject = "Wecome to Hero Traveler!",
-		msgContent = "Here's a heroic welcome message?"
-	sendEmail(userAttrs.profile.fullName, userAttrs.username, userAttrs.email, msgSubject, msgContent)
+  .then(hashedPassword => {
+  	
+  	userAttrs.password = hashedPassword
+  	return User.create(userAttrs)
+  })
+  .then(newUser => {
 
-      userAttrs.password = hashedPassword
-      console.log("userAttrs: ", userAttrs)
-      return User.create(userAttrs)
-    })
+	//send welcome email
+	sendEmail(newUser)
+	return newUser;
+    })    
 }
+
+
+
--- packages/ht-core/src/utils/emailService/index.js
+++ packages/ht-core/src/utils/emailService/index.js
@@ -1,78 +1,53 @@
 var helper = require('sendgrid').mail;
 var nodemailer = require('nodemailer');
-var handlebars = require('nodemailer-express-handlebars');
+var handlebars = require('handlebars');
 var sgTransport = require('nodemailer-sendgrid-transport');
+var cons = require('consolidate');
+
 
 var sendgridConfig = {
-  api_key : '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',
-  admin : {
-    name : "Hero Traveler",
-    email : 'james+customerservice@rehashstudio.com'
-  },
+  api_key : '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'
 }
 
-export default function sendEmail(userAttrs, msgSubject, msgContent) {
+export default function sendEmail(user) {
 
-  console.log("userAttrs: ", userAttrs)
+const context = {
+  subject: "Welcome to Hero Traveler!",
+  body: "Thank you for becoming a Hero, " + user.profile.fullName + ", let the adventure begin!",
+  logoUrl: '../Images/ht-logo-white.png',
+  logoHeight: "40px",
+  logoWidth: "120px",
+  accentColor: "blue",
+  secondaryColor: "grey",
+  siteName: "Hero Traveler",
+  }
 
-//  var options = {
-//      viewEngine: {
-//          extname: '.handlebars',
-//          layoutsDir: './templates',
-//          defaultLayout : 'emailWrapper',
-//          // partialsDir : 'views/partials/'
-//      },
-//      viewPath: './templates',
-//      extName: '.handlebars'
-//  };
+cons.handlebars(__dirname + '/templates/newUserTemplate.handlebars', context, function(err, html){
 
-// //using sendgrid as transport, but can use any transport.
-//  var send_grid = {
-//      auth: {
-//          api_key: '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'
-//      }
-//  }
-//  var mailer = nodemailer.createTransport(sgTransport(send_grid));
-//  mailer.use('compile', handlebars(options));
-//  mailer.sendMail({
-//      from: 'test@test.com',
-//      to: 'to@to.com',
-//      subject: 'Any Subject',
-//      template: 'email_body',
-//      context: {
-//           variable1 : 'value1',
-//           variable2 : 'value2'
-//      }
-//  }, function (error, response) {
-//      console.log('mail sent to ' + to);
-//      mailer.close();
-//  });  
+  if(!err) {
+    var send_grid = {
+     auth: {
+       api_key: sendgridConfig.api_key
+     }
+   }    
+var mailer = nodemailer.createTransport(sgTransport(send_grid));
+   var email = {
+    to: user.email,
+    from: 'andrew.watt@rehashstudio.com',
+    subject: 'Welcome to Hero Traveler!',
+    text: 'Hero Traveler',
+    html: html
+  };
 
+  mailer.sendMail(email, function(err, res) {
+    if (err) { 
+      console.log(err) 
+    }
+    console.log(res);
+  });
 }
 
+})
 
-
-
-
-// export default function sendEmail(fullname, username, email, msgSubject, msgContent) {
-
-//   var from_email = new helper.Email("andrew.watt@rehashstudio.com")
-//   var to_email = new helper.Email(email);
-//   var content = new helper.Content("text/plain", msgContent);
-//   var mail = new helper.Mail(from_email, msgSubject, to_email, content);
-
-//   var sg = require('sendgrid')(sendgridConfig.api_key);
-//   var request = sg.emptyRequest({
-//     method: 'POST',
-//     path: '/v3/mail/send',
-//     body: mail.toJSON()
-//   });
-
-//   sg.API(request, function(error, response) {
-//     console.log(response.statusCode);
-//     console.log(response.body);
-//     console.log(response.headers);
-//   })
-// }
-
+}
 

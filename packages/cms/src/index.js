41925c3a84e0fa6730096a8e7f4167ee0c741bec
Andrew Watt <awatt11215@gmail.com>
2017-04-24 18:05:55 -0400

    add db functionality w/ some errs to fix

--- packages/cms/src/index.js
+++ packages/cms/src/index.js
@@ -1,8 +1,23 @@
+var dotenv = require('dotenv')
+dotenv.config()
+var bodyParser = require('body-parser')
+var morgan = require('morgan')
+var db = require('@rwoody/ht-core')
+
+var initCore = db.default
+const PORT = process.env.PORT || 3000
 var path = require('path')
+
 
 const express = require('express')
 var nunjucks = require('nunjucks')
 const app = express()
+
+
+// Middleware
+app.use(bodyParser.json({}))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(morgan('dev'))
 
 const pathToTemplates = path.resolve(__dirname, 'templates')
 
@@ -12,8 +27,29 @@ nunjucks.configure(pathToTemplates, {
 })
 
 app.get('/', (req, res) => {
-  res.render('hello.html')
+
+var id = '58fe550e288aa759dbb8552f'
+
+var result = db.User.find({_id: id })
+    .then(function(result){
+    	res.render(result)
+    })
 })
 
-app.listen(3000)
-console.log('App listening on port 3000')
+initCore({
+  mongoDB: process.env.MONGODB_URL,
+  seedDB: process.env.SEED_DB,
+}).then(() => {
+  app.listen(PORT, err => {
+    if (err) {
+      console.error(err)
+    } else {
+      console.log(`App running on port ${PORT}`)
+    }
+  })
+})
+
+
+
+
+

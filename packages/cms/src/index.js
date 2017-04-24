abb945740044d997509bf84d44bb49db45ae6dad
Seth Bailey <seth.h.bailey@gmail.com>
2017-04-24 18:14:14 -0400

    nunjucks templating renders linked pages and demo data

--- packages/cms/src/index.js
+++ packages/cms/src/index.js
@@ -4,16 +4,53 @@ const express = require('express')
 var nunjucks = require('nunjucks')
 const app = express()
 
-const pathToTemplates = path.resolve(__dirname, 'templates')
+const pathToTemplates = path.resolve(__dirname, 'templates/')
+
+const userData = {
+  data: [
+    {
+      username: 'morty',
+      email: 'morty@mortymail.com',
+      createdAt: 'someDate',
+      counts: {
+        following: 33,
+        followers: 12
+      }
+    },
+    {
+      username: 'sam peckinpaw',
+      email: 'sam@thewildbunch.com',
+      createdat: 'someotherdate',
+      counts: {
+        following: 1,
+        followers: 12
+      }
+    },
+    {
+      username: 'Julia peckinpaw',
+      email: 'julia@thewildbunch.com',
+      createdat: 'somealsodate',
+      counts: {
+        following: 110000,
+        followers: 12998176
+      }
+    }
+  ]
+}
 
 nunjucks.configure(pathToTemplates, {
   autoescape: true,
-  express: app
+  express: app,
+  watch: true,
+  cache: false
+})
+
+app.get('/users', (req, res) => {
+  res.render('users.njk')
 })
 
 app.get('/', (req, res) => {
-  res.render('hello.html')
+  res.render('index.njk', userData)
 })
 
-app.listen(3000)
-console.log('App listening on port 3000')
+app.listen(3000, (req, res) => console.log('App listening on port 3000'))

ee6be57e85d3b8562322296fcd963bf918fb1d55
Seth Bailey <seth.h.bailey@gmail.com>
2017-04-27 09:46:52 -0400

    edit view working

--- packages/cms/src/index.js
+++ packages/cms/src/index.js
@@ -3,54 +3,36 @@ dotenv.config()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const User = require('@rwoody/ht-core').Models.User
+const _ = require('lodash')
 
 const initCore = require('@rwoody/ht-core').default
 const PORT = process.env.PORT || 3000
 const path = require('path')
 
-
+/* Nunjucks config */ 
 const express = require('express')
 const nunjucks = require('nunjucks')
 const app = express()
+const messages = require('express-messages')
+const flash = require('connect-flash')
+
+/* settings for template rendering */ 
 
 const pathToTemplates = path.resolve(__dirname, 'templates/')
 const resultsPerPage = 15
-const userData = {
-  data: [
-    {
-      username: 'morty',
-      email: 'morty@mortymail.com',
-      createdAt: 'someDate',
-      counts: {
-        following: 33,
-        followers: 12
-      }
-    },
-    {
-      username: 'sam peckinpaw',
-      email: 'sam@thewildbunch.com',
-      createdat: 'someotherdate',
-      counts: {
-        following: 1,
-        followers: 12
-      }
-    },
-    {
-      username: 'Julia peckinpaw',
-      email: 'julia@thewildbunch.com',
-      createdat: 'somealsodate',
-      counts: {
-        following: 110000,
-        followers: 12998176
-      }
-    }
-  ]
+const makeDateReadable = (dataArray) => {
+  return dataArray.map(element => {
+    element.createdString = element.createdAt.toDateString()
+    return element
+  })
 }
+
 
 // Middleware
 app.use(bodyParser.json({}))
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(morgan('dev'))
+app.use()
 
 
 nunjucks.configure(pathToTemplates, {
@@ -62,20 +44,34 @@ nunjucks.configure(pathToTemplates, {
 
 
 app.get('/users', (req, res) => {
-  const params = req.query
-  const page = Math.max(0, params.page || 0)
-  console.log(params)
+  const query = req.query
+  const page = Math.max(0, query.page || 0) // Defaults to zero in case query is undefined
   User.find({})
     .limit(resultsPerPage)
     .skip(page * resultsPerPage)
-    .sort({username: params.direction || 1})
+    .sort({[query.sortby]: query.direction || 1}) // Defaults to one in case query is undefined
     .then((data) => {
-      res.render('users.njk', { data, page: params.page, direction: params.direction })
+      data = makeDateReadable(data)
+      res.render('users.njk', { data, sortby: query.sortby, page, direction: query.direction })
     })
 })
 
+app.get('/edit', (req, res) => {
+  const { id } = req.query
+  User.findById(id)
+    .then((data) => res.render('edit.njk', { data }))
+})
+
+app.post('/edituser/:id', (req, res) => {
+  const { email, username, role } = req.body
+  const { id } = req.params
+  console.log(req.body)
+
+  
+})
+
 app.get('/', (req, res) => {
-  res.render('index.njk', userData )
+  res.render('index.njk')
 })
 
 // app.listen(3000, (req, res) => console.log('App listening on port 3000'))

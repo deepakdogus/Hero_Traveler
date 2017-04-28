0b469a68cf372918ba85c1a74f4c33182b8d0a8e
Ryan Wood <rwoody@gmail.com>
2017-04-28 08:26:12 -0500

    remove login info

--- packages/HeroTravelerMobile/App/Containers/LoginScreen.js
+++ packages/HeroTravelerMobile/App/Containers/LoginScreen.js
@@ -54,8 +54,8 @@ class LoginScreen extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      username: 'cngirlie2',
-      password: 'ryanwood',
+      username: '',
+      password: '',
     }
     this.isAttempting = false
   }

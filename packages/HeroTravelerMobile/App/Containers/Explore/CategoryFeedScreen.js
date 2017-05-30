4705f3d95836fbf74b8cff4bac1e11d0d31bf8c2
Ryan Wood <rwoody@gmail.com>
2017-05-30 11:14:35 -0500

    disallow video playing in feeds

--- packages/HeroTravelerMobile/App/Containers/Explore/CategoryFeedScreen.js
+++ packages/HeroTravelerMobile/App/Containers/Explore/CategoryFeedScreen.js
@@ -122,7 +122,7 @@ class CategoryFeedScreen extends React.Component {
                 storyId={storyId}
                 height={imageHeight}
                 autoPlayVideo={false}
-                allowVideoPlay={true}
+                allowVideoPlay={false}
                 onPress={() => NavActions.story({storyId})}
                 onPressLike={story => this.props.toggleLike(this.props.user.id, story.id)}
               />

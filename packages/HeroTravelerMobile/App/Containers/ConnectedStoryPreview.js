1ce5fa441e88ebf1073341469ce104fc4abbd0c3
Ryan Wood <rwoody@gmail.com>
2017-06-13 12:00:12 -0400

    Remove unncessary line

--- packages/HeroTravelerMobile/App/Containers/ConnectedStoryPreview.js
+++ packages/HeroTravelerMobile/App/Containers/ConnectedStoryPreview.js
@@ -6,7 +6,6 @@ import StoryPreview from '../Components/StoryPreview'
 const mapStateToProps = (state, ownProps) => {
   const {session: {userId}, entities} = state
   const story = entities.stories.entities[ownProps.storyId]
-  || entities.users.entities[ownProps.storyId]
   return {
     story,
     user: entities.users.entities[story.author],

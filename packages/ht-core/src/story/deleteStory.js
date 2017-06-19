6a80206d1033f78b4d5078b3f8f77fb8897c76b2
Seth Bailey <seth.h.bailey@gmail.com>
2017-06-19 16:19:32 -0400

    delete story from index

--- packages/ht-core/src/story/deleteStory.js
+++ packages/ht-core/src/story/deleteStory.js
@@ -1,5 +1,17 @@
 import {Story} from '../models'
+import algoliasearchModule from 'algoliasearch'
+
+const client = algoliasearchModule(process.env.ALGOLIA_ACCT_KEY, process.env.ALGOLIA_API_KEY)
+const storyIndex = client.initIndex(process.env.ALGOLIA_STORY_INDEX)
+
+const deleteStoryFromIndex = (storyId) => new Promise((resolve, reject) => {
+  storyIndex.deleteByQuery(storyId, (error) => {
+    if (error) reject(error)
+    else resolve('sucessfully deleted')
+  })
+})
 
 export default function deleteStory (storyId) {
   return Story.delete({_id: storyId})
+    .then(() => deleteStoryFromIndex(storyId))
 }

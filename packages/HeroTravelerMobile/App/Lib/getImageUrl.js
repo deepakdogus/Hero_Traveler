25ffd7fe70299ad7416de3216f9f3a269ea39b54
mbthiery <mbthiery@gmail.com>
2017-08-03 12:32:31 -0400

    hotfix for search crashing

--- packages/HeroTravelerMobile/App/Lib/getImageUrl.js
+++ packages/HeroTravelerMobile/App/Lib/getImageUrl.js
@@ -1,15 +1,15 @@
 import _ from 'lodash'
 import Env from '../Config/Env'
-
 export function getImageUrlBase() {
   return Env.cdnBaseUrl + 'image/upload'
 }
 
 export default function getImageUrl(image: object): ?string {
   if (!_.has(image, 'original')) return undefined
-
   const {path, folders} = image.original
   const filename = _.last(path.split('/'))
-  const url = `${getImageUrlBase()}/${folders.join('/')}/${filename}`
+  // hot fix to avoid search crashing. Need to bulk update algolia
+  const folderPath = folders ? folders.join('/') : 'files'
+  const url = `${getImageUrlBase()}/${folderPath}/${filename}`
   return url
 }

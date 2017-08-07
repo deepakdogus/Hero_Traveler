b3730a68e0e792335fe597f9597873705d44a6ce
mbthiery <mbthiery@gmail.com>
2017-08-07 10:57:42 -0400

    edit stories and new drafts now consistently load fixed problem editing existing story

--- packages/HeroTravelerMobile/App/Components/Avatar.js
+++ packages/HeroTravelerMobile/App/Components/Avatar.js
@@ -23,7 +23,7 @@ const SIZES = {
 export default class Avatar extends Component {
 
   static propTypes = {
-    size: PropTypes.oneOf(['small', 'medium', 'large']),
+    size: PropTypes.oneOf(['small', 'medium', 'large', 'extraLarge']),
     avatarUrl: PropTypes.string,
     iconColor: PropTypes.string,
   }

43a17437d204f0824d02d30c2147265c95072093
Andrew Watt <awatt11215@gmail.com>
2017-09-09 17:53:48 -0400

    add rough new date picker with red arrow icons

--- packages/web/src/App/Components/CreateStory/ReactDayPicker.js
+++ packages/web/src/App/Components/CreateStory/ReactDayPicker.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import DayPickerInput from 'react-day-picker/DayPickerInput';
+import DayPicker from 'react-day-picker';
 import './Styles/ReactDayPickerStyles.css';
+
+import Icon from '../Icon'
 
 const InputContainer = styled.div`
   display: inline-block;
@@ -10,11 +12,50 @@ const InputContainer = styled.div`
   z-index: 100;
 `
 
+const StyledIcon = styled(Icon)`
+  height: 15px;
+  width: 10px;
+`
+
+
+function Navbar({
+  nextMonth,
+  previousMonth,
+  onPreviousClick,
+  onNextClick,
+  className,
+  localeUtils,
+}) {
+  const months = localeUtils.getMonths();
+  const prev = months[previousMonth.getMonth()];
+  const next = months[nextMonth.getMonth()];
+  const styleLeft = {
+    float: 'left',
+  };
+  const styleRight = {
+    float: 'right',
+  };
+  return (
+    <div className={className}>
+      <StyledIcon 
+        name='arrowLeftRed'
+        style={styleLeft} 
+        onClick={() => onPreviousClick()}/>
+      <StyledIcon 
+        name='arrowRightRed'
+        style={styleRight} 
+        onClick={() => onNextClick()}/>
+    </div>
+  );
+}
+
 export default class ReactDayPicker extends React.Component {
   render() {
     return (
       <InputContainer>
-        <DayPickerInput className='inputStyle' placeholder='MM-DD-YYYY' format='MM-DD-YYYY' />
+        <DayPicker 
+          navbarElement={<Navbar />} 
+        />
       </InputContainer>
     )
   }

03c27f91829a44630c14af047ab6b291eb9ea416
mbthiery <mbthiery@gmail.com>
2017-09-18 14:25:38 -0400

    video thumbnails added

--- packages/shared/Lib/getImageUrl.js
+++ packages/shared/Lib/getImageUrl.js
@@ -1,5 +1,6 @@
 import _ from 'lodash'
 import Env from '../../Config/Env'
+import {getVideoUrlBase} from './getVideoUrl'
 
 export function getImageUrlBase() {
   return Env.cdnBaseUrl + 'image/upload'
@@ -8,12 +9,19 @@ export function getImageUrlBase() {
 export default function getImageUrl(image: object, type = 'cover'): ?string {
   if (!_.has(image, 'original')) return undefined
   const {path, folders} = image.original
-  const filename = _.last(path.split('/'))
+  let filename = _.last(path.split('/'))
   // hot fix to avoid search crashing. Need to bulk update algolia
   const folderPath = folders ? folders.join('/') : 'files'
   let url
   // for now we are only optimizing cover photos. Will look into possible Avatar optimizations later
   if (type === 'avatar') url = `${getImageUrlBase()}/${folderPath}/${filename}`
+  else if (type === 'video') {
+    // replacing video format with jpg to get thumbnail
+    filename = filename.split('.')
+    filename[filename.length-1] = 'jpg'
+    filename = filename.join('.')
+    url = `${getVideoUrlBase()}/${folderPath}/${filename}`
+  }
   else url = `${getImageUrlBase()}/q_auto:best,f_auto/${folderPath}/${filename}`
   return url
 }

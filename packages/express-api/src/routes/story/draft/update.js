0dda1d1db4c342f851986ecf2fd1945fdd5f8c61
mbthiery <mbthiery@gmail.com>
2017-09-19 15:42:56 -0400

    added video content streaming

--- packages/express-api/src/routes/story/draft/update.js
+++ packages/express-api/src/routes/story/draft/update.js
@@ -1,7 +1,37 @@
-import {StoryDraft} from '@hero/ht-core'
+import {StoryDraft, Models} from '@hero/ht-core'
+
+function isVideoBlock(block) {
+  return block.type === 'atomic' && block.data && block.data.type === 'video'
+}
 
 export default function updateDraft(req, res) {
   const draftId = req.params.id
   const {story: attrs} = req.body
+
+  /*
+  making sure to include the streaming urls
+  ---
+  nota bene: if they finish editing before the cloudinary callback arrives
+  they will have a normal video url instead of a streaming one. For expediency
+  not adding any further steps to correct this on callback reception
+  */
+  if (attrs.draftjsContent){
+    return Promise.all(attrs.draftjsContent.blocks.map(block => {
+      if (isVideoBlock(block) && !block.data.HLSUrl) {
+        return Models.Video.findOne({'original.path': block.data.url})
+        .then(video => {
+          const formats = video.streamingFormats
+          if (formats) {
+            if (formats.HLS) block.data.HLSUrl = formats.HLS
+            if (formats.DASH) block.data.DASHUrl = formats.DASH
+          }
+        })
+      }
+      return
+    }))
+    .then(() => {
+      return StoryDraft.update(draftId, attrs)
+    })
+  }
   return StoryDraft.update(draftId, attrs)
 }

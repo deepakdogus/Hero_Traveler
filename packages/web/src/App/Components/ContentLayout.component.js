53b9faf281eaca84fd5b192b3dd892cdc11b6539
Kevin Perumal <kevin.perumal@gmail.com>
2017-10-24 11:08:24 -0400

    modify ContentLayout component to export media query functions

--- packages/web/src/App/Components/ContentLayout.component.js
+++ packages/web/src/App/Components/ContentLayout.component.js
@@ -12,9 +12,19 @@ const sizes = {
 }
 
 // Iterate through the sizes and create a media template
-const media = Object.keys(sizes).reduce((acc, label) => {
+export const mediaMax = Object.keys(sizes).reduce((acc, label) => {
   acc[label] = (...args) => css`
     @media (max-width: ${sizes[label] / 16}em) {
+      ${css(...args)}
+    }
+  `
+
+  return acc
+}, {})
+
+export const mediaMin = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (min-width: ${sizes[label] / 16}em) {
       ${css(...args)}
     }
   `
@@ -25,15 +35,15 @@ const media = Object.keys(sizes).reduce((acc, label) => {
 const ContentDiv = styled.div`
   margin: 80px;
 
-  ${media.desktop`
+  ${mediaMax.desktop`
     margin: 40px;
   `}
 
-  ${media.phone`
+  ${mediaMax.phone`
     margin: 10px;
   `}
 
-  ${media.tablet`
+  ${mediaMax.tablet`
     margin: 10px;
   `}
 `

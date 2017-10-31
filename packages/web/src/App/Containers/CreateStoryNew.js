d315728e42f559b117ed2968e11418dc5abd3174
mbthiery <mbthiery@gmail.com>
2017-10-31 16:23:52 -0400

    added coverContent validation

--- packages/web/src/App/Containers/CreateStoryNew.js
+++ packages/web/src/App/Containers/CreateStoryNew.js
@@ -4,6 +4,7 @@ import {Route} from 'react-router-dom'
 import {connect} from 'react-redux'
 import {push} from 'react-router-redux';
 import _ from 'lodash'
+import Modal from 'react-modal'
 
 import StoryCreateActions from '../Shared/Redux/StoryCreateRedux'
 import API from '../Shared/Services/HeroAPI'
@@ -11,6 +12,7 @@ import API from '../Shared/Services/HeroAPI'
 import CreateStoryCoverContent from './CreateStory/1_CoverContent'
 import CreateStoryDetails from './CreateStory/2_Details'
 import FooterToolbar from '../Components/CreateStory/FooterToolbar'
+import {Title, Text} from '../Components/Modals/Shared'
 
 const api = API.create()
 
@@ -29,11 +31,29 @@ function isNew(props) {
   return draftId === 'new'
 }
 
+const customModalStyles = {
+  content: {
+    width: 420,
+    margin: 'auto',
+  },
+  overlay: {
+    backgroundColor: 'rgba(0,0,0, .5)',
+    zIndex: 100,
+  }
+}
+
 /*
 this container is in charge of creating/loading the appropriate draft
 and dealing with update logic
 */
 class CreateStory extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      error: {},
+    }
+  }
+
   componentWillMount() {
     const draftId = this.props.match.params.draftId
     if (isNew(this.props)) {
@@ -62,7 +82,7 @@ class CreateStory extends Component {
       .then(response => response.data)
     }
     else coverPromise = Promise.resolve(workingDraft)
-    coverPromise.then(response => {
+    return coverPromise.then(response => {
       const cleanedWorkingDraft = _.omit(workingDraft, 'tempCover')
       this.props.updateDraft(draft.id, cleanedWorkingDraft)
     })
@@ -73,13 +93,54 @@ class CreateStory extends Component {
   }
 
   onRight = () => {
-    // add some saving logic
-    const {reroute, draft} = this.props
-    reroute(`/createStoryNew/${draft.id}/details`)
+    const subPath = this.getSubPath(this.props.location)
+    if (subPath === 'cover') this.coverContentOnRight()
+    else if (subPath === 'details') this.detailsOnRight()
+  }
+
+  getSubPath(location) {
+    const splitPath = location.pathname.split('/')
+    return splitPath[splitPath.length - 1]
+  }
+
+  setValidationErrorState = (text) => {
+    this.setState({
+      error: {
+        title: 'Validation Error',
+        text,
+      }
+    })
+  }
+
+  coverContentOnRight = () => {
+    const {workingDraft, reroute, draft} = this.props
+    if (!workingDraft.title || !this.hasCover()) {
+      this.setValidationErrorState('Please include story cover and title')
+    }
+    else {
+      this._updateDraft()
+      .then(() => reroute(`/createStoryNew/${draft.id}/details`))
+    }
+  }
+
+  detailsOnRight = () => {
+    if (!this.props.workingDraft.type) {
+      this.setValidationErrorState('Please include an activity')
+    }
+  }
+
+  hasCover = () => {
+    const {workingDraft} = this.props
+    return workingDraft.tempCover || workingDraft.coverImage || workingDraft.coverVideo
+  }
+
+  closeModal = () => {
+    this.setState({error: {}})
   }
 
   render() {
     const {workingDraft, match} = this.props
+    const error = this.state.error
     return (
       <Container>
         <ContentWrapper>
@@ -102,6 +163,15 @@ class CreateStory extends Component {
           isDetailsView={false}
           onRight={this.onRight}
         />
+        <Modal
+          isOpen={!!error.title}
+          contentLabel="Signup Modal"
+          onRequestClose={this.closeModal}
+          style={customModalStyles}
+        >
+          <Title>{error.title}</Title>
+          <Text>{error.text}</Text>
+        </Modal>
       </Container>
     )
   }
@@ -115,6 +185,7 @@ function isAccessToken(token){
 function mapStateToProps(state) {
   const accessToken = state.session.tokens.find(isAccessToken) || {}
   return {
+    location: state.routes.location,
     accessToken: accessToken.value,
     draft: state.storyCreate.draft,
     workingDraft: state.storyCreate.workingDraft,

30a9c3e77ea1ff7211cf040ddd74158aba8d312a
mbthiery <mbthiery@gmail.com>
2017-11-01 16:17:37 -0400

    connected story view

--- packages/web/src/App/Containers/Story.js
+++ packages/web/src/App/Containers/Story.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
-import {feedExample, usersExample} from './Feed_TEST_DATA'
+import {feedExample} from './Feed_TEST_DATA'
+import StoryActions from '../Shared/Redux/Entities/Stories'
+
 import Header from '../Components/Header'
 import StoryHeader from '../Components/StoryHeader'
 import StoryContentRenderer from '../Components/StoryContentRenderer'
@@ -24,10 +28,23 @@ const GreyWrapper = styled.div`
 `
 
 class Story extends Component {
+  static propTypes = {
+    story: PropTypes.object,
+    author: PropTypes.object,
+    getStory: PropTypes.func,
+    match: PropTypes.object,
+  }
+
+  componentWillMount() {
+    if (!this.props.story) {
+      this.props.getStory(this.props.match.params.storyId)
+    }
+  }
+
   render() {
-    const {match} = this.props
-    const story = feedExample[match.params.storyId]
-    const author = usersExample[story.author]
+    const {story, author} = this.props
+    if (!story || !author) return null
+
     return (
       <ContentWrapper>
         <Header isLoggedIn></Header>
@@ -57,4 +74,20 @@ class Story extends Component {
   }
 }
 
-export default Story
+function mapStateToProps(state, ownProps) {
+  const storyId = ownProps.match.params.storyId
+  const story = state.entities.stories.entities[storyId]
+  const author = story ? state.entities.users.entities[story.author] : undefined
+  return {
+    story,
+    author,
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    getStory: (storyId, tokens) => dispatch(StoryActions.storyRequest(storyId)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Story)

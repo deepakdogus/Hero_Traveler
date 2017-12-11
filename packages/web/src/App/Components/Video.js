638ed4494bd1a641dfb85854f2ac0407d4d39a8c
Kat Mello <kat.mello@rehashstudio.com>
2017-12-11 15:07:09 -0500

    Changes max height

--- packages/web/src/App/Components/Video.js
+++ packages/web/src/App/Components/Video.js
@@ -6,17 +6,17 @@ const VideoWrapper = styled.figure`
   text-align: center;
   background-color: ${props => props.theme.Colors.background};
   margin: 0;
-  max-height: ${props => props.type === 'cover' ? '60vh' : '700px'};
+  max-height: ${props => props.type === 'cover' ? '60vh' : '600px'};
 `
 
 const StyledVideo = styled.video`
-  max-height: ${props => props.type === 'cover' ? '60vh' : '700px'};
+  max-height: ${props => props.type === 'cover' ? '60vh' : '600px'};
   max-width: 100%;
 `
 
 export default class Video extends React.Component {
   static propTypes = {
-    src: PropTypes.string,
+    src: PropTypes.string6
     type: PropTypes.oneOf(['cover', 'preview']),
     noControls: PropTypes.bool,
   }

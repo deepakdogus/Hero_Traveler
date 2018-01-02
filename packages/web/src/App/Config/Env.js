480929b2be16b518a926fb01c1e8db9319491a49
Kat Mello <kat.mello@rehashstudio.com>
2018-01-02 15:48:34 -0500

    Reverts config file

--- packages/web/src/App/Config/Env.js
+++ packages/web/src/App/Config/Env.js
@@ -1,17 +1,17 @@
 // these are all public keys so they can be exposed on the front-end
 const devSettings = {
   API_URL: 'http://localhost:3000/',
-  cdnBaseUrl: 'https://res.cloudinary.com/duuyquprs/',
-  cloudName: 'duuyquprs',
-  imagePreset: 'aurqaif8',
-  videoPreset: 'fhcwbr7j',
+  cdnBaseUrl: 'https://res.cloudinary.com/di6rehjqx/',
+  cloudName: 'di6rehjqx',
+  imagePreset: 'xoj70ai1',
+  videoPreset: 'qriodesx',
 
   SEARCH_APP_NAME: 'BEEW4KQKOP',
   SEARCH_API_KEY: '318fa67b79fd7fdd680d68798d666786',
-  SEARCH_STORY_INDEX: 'kat_dev_STORIES',
-  SEARCH_USER_INDEX: 'kat_dev_USERS',
-  SEARCH_CATEGORIES_INDEX: 'kat_dev_CATEGORIES',
-} 
+  SEARCH_STORY_INDEX: 'matthew_dev_STORIES',
+  SEARCH_USER_INDEX: 'matthew_dev_USERS',
+  SEARCH_CATEGORIES_INDEX: 'matthew_dev_CATEGORIES',
+}
 
 const prodSettings = {
   API_URL: 'http://ht-api.rehashstudio.com/',

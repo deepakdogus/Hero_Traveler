e562cb7e43877754f86fd94b66a56bf461a66745
Kat Mello <kat.mello@rehashstudio.com>
2018-01-16 15:14:43 -0500

    Navbar gets black background on scroll

--- packages/web/src/App/Containers/Header.js
+++ packages/web/src/App/Containers/Header.js
@@ -10,7 +10,9 @@ import LoginActions from '../Shared/Redux/LoginRedux'
 import UXActions from '../Redux/UXRedux'
 import HeaderModals from '../Components/HeaderModals'
 
-const StyledGrid = styled(Grid)`
+// If we don't explicity prevent 'fixed' from being passed to Grid, we get an error about unknown prop on div element
+// because apparently react-flexbox-grid passes all props down to underlying React elements
+const StyledGrid = styled(({ fixed, ...rest }) => <Grid {...rest} />)`
   padding: 15px;
   z-index: 3;
   position: ${props => props.fixed ? 'fixed' : 'absolute'};
@@ -42,6 +44,26 @@ class Header extends React.Component {
     super(props)
     this.state = {
       modal: undefined,
+      navbarEngaged: false,
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    // If header is transparent, it should mark itself as "engaged" so we know to style it differently (aka black background)
+    if (!this.props.blackHeader){
+      if (window.scrollY > 65 && !this.state.navbarEngaged){
+        this.setState({ navbarEngaged: true })
+      } else if (window.scrollY < 65 && this.state.navbarEngaged) {
+        this.setState({ navbarEngaged: false })
+      }
     }
   }
 
@@ -54,6 +76,7 @@ class Header extends React.Component {
   }
 
   componentWillReceiveProps(nextProps) {
+     
     if (!this.props.isLoggedIn && nextProps.isLoggedIn) this.closeModal()
   }
 
@@ -72,7 +95,7 @@ class Header extends React.Component {
 
   render () {
     const {isLoggedIn, attemptLogin, closeGlobalModal, currentUser, globalModalThatIsOpen, globalModalParams } = this.props
-    const SelectedGrid = this.props.blackHeader ? StyledGridBlack : StyledGrid
+    const SelectedGrid = (this.props.blackHeader || this.state.navbarEngaged) ? StyledGridBlack : StyledGrid
     const spacerSize = this.props.blackHeader ? '65px' : '0px'
     return (
       <div>

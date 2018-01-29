7ca5597a4d06c34124fc64d724bfd554625484ad
mbthiery <mbthiery@gmail.com>
2018-01-29 15:39:19 -0500

    cleanup

--- packages/shared/Lib/firstTimeTooltips.js
+++ packages/shared/Lib/firstTimeTooltips.js
@@ -2,7 +2,6 @@ import _ from 'lodash'
 
 export const Types = {
   STORY_CREATE: 'story_create',
-  STORY_PHOTO_TAKE: 'story_photo_take',
   STORY_PHOTO_NEXT: 'story_photo_next',
   STORY_PHOTO_EDIT: 'story_photo_edit',
   PROFILE_NO_STORIES: 'profile_no_stories',

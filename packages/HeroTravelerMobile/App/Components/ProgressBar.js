f920cd84ac29fb0d94ce82677aa922a659d9183e
Andrew Beck <abeck99@gmail.com>
2018-02-27 18:11:06 -0500

    Remove React.PropTypes references

--- packages/HeroTravelerMobile/App/Components/ProgressBar.js
+++ packages/HeroTravelerMobile/App/Components/ProgressBar.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import {Text, TouchableOpacity, ProgressViewIOS, View} from 'react-native'
 import styles from './Styles/ProgressBarStyles'
 import {Colors} from '../Shared/Themes'

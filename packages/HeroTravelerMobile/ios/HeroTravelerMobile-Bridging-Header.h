a142aa14de843c4bf4b3d966319086404730f15e
Andrew Beck <abeck99@gmail.com>
2018-03-06 15:06:47 -0500

    Add Video Processing dependency

---
+++ packages/HeroTravelerMobile/ios/HeroTravelerMobile-Bridging-Header.h
@@ -0,0 +1,9 @@
+#ifndef HeroTravelerMobile_Bridging_Header_h
+#define HeroTravelerMobile_Bridging_Header_h
+
+#import <React/RCTRootView.h>
+#import <React/RCTView.h>
+#import <React/RCTBridgeModule.h>
+#import "RNVideoProcessing.h"
+
+#endif /* HeroTravelerMobile_Bridging_Header_h */

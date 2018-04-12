1a309e170d666897134c3c726b2b7e67b659eedc
mbthiery <mbthiery@gmail.com>
2018-04-12 15:33:05 -0400

    videos display regardless for dev

--- packages/express-api/src/utils/removeStreamlessStories.js
+++ packages/express-api/src/utils/removeStreamlessStories.js
@@ -1,6 +1,7 @@
 import _ from 'lodash'
 
 export default function removeStreamlessStories(stories) {
+  if (process.env.NODE_ENV === 'development') return stories
   return stories.filter((story, index) => {
     if (story.coverVideo && !_.get(story, 'coverVideo.streamingFormats.HLS')) {
       return false

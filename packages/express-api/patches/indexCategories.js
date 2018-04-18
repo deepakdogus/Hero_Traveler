b590d6994256684adc477c23c353157be8e9b358
mbthiery <mbthiery@gmail.com>
2018-04-18 14:43:35 -0400

    cleanup + commenting

--- packages/express-api/patches/indexCategories.js
+++ packages/express-api/patches/indexCategories.js
@@ -1,23 +1,7 @@
 import {Models} from '../../ht-core'
 import {algoliaHelper} from '@hero/ht-util'
 
-const client = algoliasearchModule(process.env.ALGOLIA_ACCT_KEY, process.env.ALGOLIA_API_KEY)
-const categoryIndex = client.initIndex(process.env.ALGOLIA_CATEGORY_INDEX)
-
 export default function indexCategories(){
   Models.Category.find({isDefault: false})
-  .then(categories => {
-    const formattedCategories = categories.map(category => {
-      return {
-        _id: category._id,
-        title: category.title,
-      }
-    })
-    return new Promise((resolve, reject) => {
-      categoryIndex.addObjects(formattedCategories, (error, content)=> {
-        if (error) reject(error)
-        else resolve(content)
-      })
-    })
-  })
+  .then(algoliaHelper.addCategoriesToIndex)
 }
--- packages/ht-core/src/story/deleteStory.js
+++ packages/ht-core/src/story/deleteStory.js
@@ -2,6 +2,7 @@ import {Story} from '../models'
 import {algoliaHelper} from '@hero/ht-util'
 
 export default function deleteStory (storyId) {
+  // intentionally doing algolia delete first to prevent client side search crash
   return algoliaHelper.deleteStoryFromIndex(storyId)
   .then(() => Story.delete({_id: storyId}))
 }

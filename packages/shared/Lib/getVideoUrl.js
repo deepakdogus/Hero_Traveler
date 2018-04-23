576f2f6e838392315216e490de41edd1cc2dd8b2
mbthiery <mbthiery@gmail.com>
2018-04-23 16:21:54 -0700

    added 10 minute buffer before enabling streams

--- packages/shared/Lib/getVideoUrl.js
+++ packages/shared/Lib/getVideoUrl.js
@@ -1,4 +1,5 @@
 import _ from 'lodash'
+import moment from 'moment'
 import Env from '../../Config/Env'
 
 export function getVideoUrlBase() {
@@ -13,10 +14,18 @@ export default function getVideoUrl(video: object, stream = true): ?string {
     (video.substring(0,7) === 'file://' || video.substring(0,6) === '/Users')
   ) return video
 
+  // if a video is under 10 minutes long we force the mp4 version
+  // this ensures we have enough time to prepare all the streamingFormats
+  const streamBufferTime = moment().subtract(10, 'minutes')
+  const timeFromBuffer = streamBufferTime.to(moment(video.createdAt))
+  const bufferHasPassed = timeFromBuffer.split(" ")[2] === 'ago'
 
   if (!_.has(video, 'original')) return undefined
   let url
-  if (stream && video.streamingFormats && video.streamingFormats.HLS) {
+  if (
+    bufferHasPassed && stream
+    && video.streamingFormats && video.streamingFormats.HLS
+  ) {
     const {folders} = video.original
     const filename = _.last(video.streamingFormats.HLS.split('/'))
     url = `${getVideoUrlBase()}/sp_full_hd/${folders.join('/')}/${filename}`

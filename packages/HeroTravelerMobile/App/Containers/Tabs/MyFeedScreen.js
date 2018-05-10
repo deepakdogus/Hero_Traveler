11e5e26bfcc7cae38d02be8f870296e18f61dd8e
mbthiery <mbthiery@gmail.com>
2018-05-10 16:35:31 -0400

    shortcircuit manual refresh too

--- packages/HeroTravelerMobile/App/Containers/Tabs/MyFeedScreen.js
+++ packages/HeroTravelerMobile/App/Containers/Tabs/MyFeedScreen.js
@@ -30,14 +30,15 @@ class MyFeedScreen extends React.Component {
   }
 
   componentDidMount() {
-    const {sync} = this.props
-    // do not want to get feed if we are doing an update
-    const shouldGetFeed = !(
-      sync.syncProgressSteps && !sync.error
-      && sync.syncProgressSteps !== sync.syncProgress
-    )
-    if (shouldGetFeed) this.props.attemptGetUserFeed(this.props.userId)
+    if (!this.isPendingUpdate()) this.props.attemptGetUserFeed(this.props.userId)
     SplashScreen.hide()
+  }
+
+  isPendingUpdate() {
+    const {sync} = this.props
+    return  sync.syncProgressSteps
+    && sync.syncProgressSteps !== sync.syncProgress
+    && !sync.error
   }
 
   isSuccessfulLoad(nextProps){
@@ -89,6 +90,7 @@ class MyFeedScreen extends React.Component {
   }
 
   _onRefresh = () => {
+    if (this.isPendingUpdate()) return
     this.setState({refreshing: true})
     this.props.attemptGetUserFeed(this.props.user.id)
   }

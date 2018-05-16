7b5d6ea411b162f96c070923eff089c9f1cd69ab
mbthiery <mbthiery@gmail.com>
2018-05-16 14:11:57 -0400

    fixed post-delete crash on Story Search result click

--- packages/HeroTravelerMobile/App/Containers/Tabs/ExploreScreen.js
+++ packages/HeroTravelerMobile/App/Containers/Tabs/ExploreScreen.js
@@ -18,7 +18,6 @@ import AlgoliaSearchHelper from 'algoliasearch-helper';
 import CategoryActions from '../../Shared/Redux/Entities/Categories'
 import Loader from '../../Components/Loader'
 import ExploreGrid from '../../Components/ExploreGrid'
-import {Metrics} from '../../Shared/Themes'
 import styles, {CategoryFeedNavActionStyles} from '../Styles/ExploreScreenStyles'
 import Colors from '../../Shared/Themes/Colors'
 import List from '../../Components/List'
@@ -57,6 +56,34 @@ class ExploreScreen extends Component {
 
   componentWillUnmount() {
     this.removeSearchListeners(this.helper)
+  }
+
+  // after we delete a story we need to make sure we purge that story
+  // from the story's hits
+  componentWillReceiveProps(nextProps) {
+    const oldStories = this.props.stories
+    const {selectedTabIndex, lastSearchResults} = this.state
+    const hits = lastSearchResults ? lastSearchResults.hits : []
+    if (
+      selectedTabIndex === 0
+      && hits.length
+      && this.hasDeletedStory(nextProps)
+    ) {
+      for (let key in oldStories) {
+        if (!nextProps.stories[key]) {
+          lastSearchResults.hits = hits.filter(hit => {
+            return key !== hit._id
+          })
+          this.setState({lastSearchResults})
+        }
+      }
+    }
+  }
+
+  hasDeletedStory(nextProps) {
+    const oldLength = Object.keys(this.props.stories).length
+    const newLength = Object.keys(nextProps.stories).length
+    return oldLength - newLength === 1
   }
 
   setupSearchListeners(helper) {
@@ -157,6 +184,7 @@ class ExploreScreen extends Component {
   renderSearchSection() {
     let searchHits = _.get(this.state.lastSearchResults, 'hits', [])
     const isSearching = this.state.searching
+
     return (
       <View style={styles.tabs}>
         <View style={styles.tabnav}>
@@ -354,6 +382,7 @@ const mapStateToProps = (state) => {
   return {
     user: state.entities.users.entities[state.session.userId],
     users: state.entities.users.entities,
+    stories: state.entities.stories.entities,
     categories,
     categoriesFetchStatus,
     error: categoriesError

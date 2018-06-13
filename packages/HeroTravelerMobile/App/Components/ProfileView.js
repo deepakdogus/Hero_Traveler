a4b4a53bfa52c2b6e54c78c8c0eb620df3f0eca1
erkmene <erkmene@gmail.com>
2018-06-13 13:46:19 +0300

    Remove all edit references from profile view.

--- packages/HeroTravelerMobile/App/Components/ProfileView.js
+++ packages/HeroTravelerMobile/App/Components/ProfileView.js
@@ -2,11 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './Styles/ProfileViewStyles'
 import {
-  View,
-  ScrollView,
-  Text,
-  TextInput,
-  TouchableOpacity,
+  View
 } from 'react-native'
 import { connect } from 'react-redux'
 import { Actions as NavActions } from 'react-native-router-flux'
@@ -21,10 +17,7 @@ import Tooltip from './Tooltip'
 
 // @TODO UserActions shouldn't be in a component
 import UserActions from '../Shared/Redux/Entities/Users'
-import StoryCreateActions from '../Shared/Redux/StoryCreateRedux'
 import isTooltipComplete, {Types as TooltipTypes} from '../Shared/Lib/firstTimeTooltips'
-import Loader from '../Components/Loader'
-import {Colors} from '../Shared/Themes'
 
 const api = HeroAPI.create()
 
@@ -34,12 +27,6 @@ export const TabTypes = {
   bookmarks: 'TAB_BOOKMARKS',
 }
 
-const usernameContansts = {
-  maxLength: 20,
-  minLength: 2,
-}
-
-// @TOOO make this smaller
 class ProfileView extends React.Component {
 
   static defaultProps = {
@@ -108,14 +95,6 @@ class ProfileView extends React.Component {
     })
   }
 
-  _onRight = () => {
-    if (this.state.usernameText.length <= 1) {
-      this.setState({error: `Usernames must be between ${usernameContansts.minLength} and ${usernameContansts.maxLength} characters`})
-      return
-    }
-    this._updateUser();
-  }
-
   _updateUser = () => {
     this.props.updateUser({
       bio: this.state.bioText,
@@ -126,27 +105,6 @@ class ProfileView extends React.Component {
   }
 
   _onLeft = () => {
-    const {id, profile} = this.props.user
-
-    // currently tempCover and tempAvatar are actually directly saved to DB - so we need to revert
-    // need to add fullName so that we dont accidentally set it to undefined
-    const profileReverts = {
-      fullName: profile.fullName,
-      cover: profile.cover,
-      avatar: profile.avatar,
-    }
-
-    this.props.updateUser({
-      profile: profileReverts,
-    })
-
-    this.props.updateUserSuccess({
-      id,
-      profile: {
-        tempCover: undefined,
-        tempAvatar: undefined,
-      }
-    })
     NavActions.pop()
   }
 
@@ -189,14 +147,13 @@ class ProfileView extends React.Component {
 
   renderProfileInfo = () => {
     const {
-      user, editable, isEditing,
+      user, editable,
       isFollowing, onPressFollow, onPressUnfollow
     } = this.props
     return (
       <ProfileUserInfo
         user={user}
         editable={editable}
-        isEditing={isEditing}
         isFollowing={isFollowing}
         onPressUnfollow={onPressUnfollow}
         onPressFollow={onPressFollow}
@@ -224,61 +181,28 @@ class ProfileView extends React.Component {
   }
 
   render() {
-    const {editable, isEditing, location, stories} = this.props
+    const {editable, location, stories} = this.props
 
-    let showTooltip = !isEditing && editable &&
+    let showTooltip = editable &&
       !stories.length && !this.hasCompletedNoStoriesTooltip()
 
     return (
       <View style={styles.flexOne}>
-        {isEditing &&
-          <NavBar
-            title='Edit Profile'
-            leftTitle='Cancel'
-            onLeft={this._onLeft}
-            rightTitle='Save'
-            onRight={this._onRight}
-            style={styles.navbarStyle}
-          />
-        }
-
         <View style={styles.gradientWrapper}>
-          {isEditing &&
-            <ScrollView style={styles.flexOne}>
-              <View style={styles.flexOne}>
-                {this.renderProfileInfo()}
-                <View style={styles.bioWrapper}>
-                  <Text style={styles.editBio}>Bio</Text>
-                  <TextInput
-                    ref={this._bioRef}
-                    style={styles.bioText}
-                    multiline
-                    onChangeText={this._setBioText}
-                    value={this.state.bioText}
-                    maxLength={500}
-                    returnKeyType={'done'}
-                    placeholder='Tell us about yourself!'
-                  />
-                </View>
-              </View>
-            </ScrollView>
-          }
-          {!isEditing &&
-            <ProfileTabsAndStories
-              editable={editable}
-              renderProfileInfo={this.renderProfileInfo}
-              storiesById={this.getStoriesById()}
-              fetchStatus={this.getFetchStatus()}
-              tabTypes={TabTypes}
-              selectTab={this.selectTab}
-              selectedTab={this.state.selectedTab}
-              user={this.props.user}
-              sessionUserId={this.props.userId}
-              showTooltip={showTooltip}
-              location={location}
-              error={this.getProfileTabsAndStoriesError()}
-            />
-          }
+          <ProfileTabsAndStories
+            editable={editable}
+            renderProfileInfo={this.renderProfileInfo}
+            storiesById={this.getStoriesById()}
+            fetchStatus={this.getFetchStatus()}
+            tabTypes={TabTypes}
+            selectTab={this.selectTab}
+            selectedTab={this.state.selectedTab}
+            user={this.props.user}
+            sessionUserId={this.props.userId}
+            showTooltip={showTooltip}
+            location={location}
+            error={this.getProfileTabsAndStoriesError()}
+          />
         </View>
         {this.state.error &&
           <ShadowButton

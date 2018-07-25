c5121daf434ecc85305484dbb6b6ef34c31690a5
mbthiery <mbthiery@gmail.com>
2018-07-25 12:07:05 -0400

    fixing typo

--- packages/ht-core/src/guide/bulkSaveStoryToGuide.js
+++ packages/ht-core/src/guide/bulkSaveStoryToGuide.js
@@ -12,7 +12,7 @@ export default async function bulkSaveStoryToGuide(storyId, isInGuide) {
     return Promise.all(guides.map(guide => {
       if (isInGuide[guide.id] && guide.stories.indexOf(storyId) === -1) {
         if (guide.stories.length) {
-          guide.stories = [...guide.storyies, storyId]
+          guide.stories = [...guide.stories, storyId]
         }
         else guide.stories = [storyId]
         return guide.save()

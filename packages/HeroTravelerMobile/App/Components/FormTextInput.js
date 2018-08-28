51830018d7d7659a84bd92b81015e5fe5c66cec6
bryan clark <bryanhclark@gmail.com>
2018-08-28 13:04:59 -0400

    added maxlength to about field in profileEdit

--- packages/HeroTravelerMobile/App/Components/FormTextInput.js
+++ packages/HeroTravelerMobile/App/Components/FormTextInput.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import {View, Text, TextInput} from 'react-native';
+import PropTypes from 'prop-types'
 
 export class FormTextInput extends React.Component {
+
+  static propTypes = {
+    input: PropTypes.object,
+    meta: PropTypes.object,
+    label: PropTypes.string,
+    multiline: PropTypes.bool,
+    placeholder: PropTypes.string,
+    styles: PropTypes.object,
+    maxLength: PropTypes.number,
+    returnKeyType: PropTypes.string,
+  }
 
   // This component is designed to be used in conjunction
   // with redux-form, so most of the parameters (styles, etc.) come
@@ -16,7 +28,14 @@ export class FormTextInput extends React.Component {
   }
 
   render() {
-    const {input, meta, label, styles} = this.props;
+    const {
+      input,
+      meta,
+      label,
+      styles,
+      maxLength
+    } = this.props;
+
     return (
       <View style={styles.inputWrapper}>
         {label && <Text style={styles.inputLabel}>{label}</Text>}
@@ -33,6 +52,7 @@ export class FormTextInput extends React.Component {
           autoCapitalize={this.props.autoCapitalize}
           secureTextEntry={this.props.secureTextEntry}
           keyboardType={this.props.keyboardType}
+          maxLength={maxLength}
         />
         {!meta.pristine && !meta.active && meta.error &&
           <View style={styles.errorView}>

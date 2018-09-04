3a904aa67688ba1fbd34be08d5a9068dedf1cc1f
mbthiery <mbthiery@gmail.com>
2018-09-04 16:38:17 -0400

    reset to prodSettings

--- packages/web/src/App/Config/Env.js
+++ packages/web/src/App/Config/Env.js
@@ -1,6 +1,6 @@
 // these are all public keys so they can be exposed on the front-end
 const devSettings = {
-  API_URL: 'http://localhost:3000/',
+  API_URL: 'http://localhost:3000',
   cdnBaseUrl: 'https://res.cloudinary.com/di6rehjqx/',
   cloudName: 'di6rehjqx',
   imagePreset: 'xoj70ai1',
@@ -14,23 +14,9 @@ const devSettings = {
   SEARCH_HASHTAGS_INDEX:'matthew_dev_HASHTAGS',
 }
 
-const mixedSettings = {
-  API_URL: 'https://95189b2d.ngrok.io',
-  cdnBaseUrl: 'https://res.cloudinary.com/rehash-studio/',
-  cloudName: 'rehash-studio',
-  imagePreset: 'xoaf1z88',
-  videoPreset: 'frbe5p09',
-
-  SEARCH_APP_NAME: 'BEEW4KQKOP',
-  SEARCH_API_KEY: '318fa67b79fd7fdd680d68798d666786',
-  SEARCH_STORY_INDEX: 'prod_STORIES',
-  SEARCH_USER_INDEX: 'prod_USERS',
-  SEARCH_CATEGORIES_INDEX: 'prod_CATEGORIES',
-  SEARCH_HASHTAGS_INDEX:'prod_HASHTAGS',
-}
 
 const prodSettings = {
-  API_URL: 'https://ht-api.rehashstudio.com/',
+  API_URL: 'https://ht-api.rehashstudio.com',
   cdnBaseUrl: 'https://res.cloudinary.com/rehash-studio/',
   cloudName: 'rehash-studio',
   imagePreset: 'xoaf1z88',
@@ -43,4 +29,4 @@ const prodSettings = {
   SEARCH_CATEGORIES_INDEX: 'prod_CATEGORIES',
 }
 
-export default process.env.NODE_ENV === 'development' ? mixedSettings : mixedSettings
+export default process.env.NODE_ENV === 'development' ? prodSettings : prodSettings

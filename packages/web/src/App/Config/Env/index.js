c1b8e7b0655b8e35e57ace7b3d89beca4e42d0ea
mbthiery <mbthiery@gmail.com>
2018-09-05 17:14:51 -0400

    cleanup

--- packages/web/src/App/Config/Env/index.js
+++ packages/web/src/App/Config/Env/index.js
@@ -1,8 +1,4 @@
 import devSettings from './dev'
 import prodSettings from './prod'
 
-console.log("devSettings", devSettings)
-console.log("prodSettings", prodSettings)
-console.log("settings", process.env.NODE_ENV === 'development' ? devSettings : prodSettings)
-
 export default process.env.NODE_ENV === 'development' ? devSettings : prodSettings

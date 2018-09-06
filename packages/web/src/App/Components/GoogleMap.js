ee660ebe84374f5600c8adf104eeb45d2723d0be
mbthiery <mbthiery@gmail.com>
2018-09-06 15:33:01 -0400

    user can see a properly centered and zoomed map of a guide's stories

--- packages/web/src/App/Components/GoogleMap.js
+++ packages/web/src/App/Components/GoogleMap.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import MarkerClusterer from "react-google-maps/lib/addons/MarkerClusterer"
 import _ from 'lodash'
+const windowMaps = window.google.maps
 
-const GettingStartedGoogleMap = withGoogleMap(props => {
+const HOCMap = withGoogleMap(props => {
   return (
     <GoogleMap
       ref={props.onMapLoad}
@@ -10,42 +13,89 @@ const GettingStartedGoogleMap = withGoogleMap(props => {
       defaultCenter={{ lat: props.lat, lng: props.lng }}
       onClick={props.onMapClick}
     >
-      {props.markers && props.markers.map((marker, index) => (
-        <Marker
-          {...marker}
-          onRightClick={() => props.onMarkerRightClick(index)}
-        />
-      ))}
+      <MarkerClusterer averageCenter>
+        {props.markers && props.markers.map((marker, index) => (
+          <Marker
+            {...marker}
+            onRightClick={() => props.onMarkerRightClick(index)}
+          />
+        ))}
+      </MarkerClusterer>
     </GoogleMap>
   )}
 );
 
 export default class GMap extends React.Component {
+  static propTypes = {
+    stories: PropTypes.arrayOf(PropTypes.object),
+  }
+
+  getMarkers() {
+    return this.props.stories.map((story, index) => {
+      return {
+        position: {
+          lat: story.locationInfo.latitude,
+          lng: story.locationInfo.longitude,
+        },
+        key: `${story.locationInfo.name}-${index}`,
+        defaultAnimation: 2,
+      }
+    })
+  }
+
+  getBounds() {
+    const bounds = new windowMaps.LatLngBounds()
+
+    this.props.stories.forEach(story => {
+      const {latitude, longitude} = story.locationInfo
+      bounds.extend(
+        new windowMaps.LatLng(
+          latitude,
+          longitude,
+        )
+      )
+    })
+
+    // edge case for single story so we are not overly zoomed in
+    if (bounds.getNorthEast().equals(bounds.getSouthWest())) {
+      var extendPointNE = new windowMaps.LatLng(
+        bounds.getNorthEast().lat() + 0.01,
+        bounds.getNorthEast().lng() + 0.01
+      )
+      var extendPointSW =  new windowMaps.LatLng(
+        bounds.getNorthEast().lat() - 0.01,
+        bounds.getNorthEast().lng() - 0.01
+      )
+      bounds.extend(extendPointNE)
+      bounds.extend(extendPointSW)
+    }
+
+    return bounds
+  }
+
+  onMapLoad = (ref) => {
+    this.gMapRef = ref
+    if (this) {
+      ref.fitBounds(this.getBounds())
+    }
+  }
 
   render () {
-    const {lat, lng, location} = this.props
-    const markers = [{
-      position: {
-        lat: lat,
-        lng: lng,
-      },
-      key: `${location}`,
-      defaultAnimation: 2,
-    }]
     return (
-      <GettingStartedGoogleMap
-        lat={lat}
-        lng={lng}
+      <HOCMap
+        lat={this.props.stories[0].locationInfo.latitude}
+        lng={this.props.stories[0].locationInfo.longitude}
         containerElement={
           <div style={{ height: `500px` }} />
         }
         mapElement={
           <div style={{ height: `100%` }} />
         }
-        onMapLoad={_.noop}
+        onMapLoad={this.onMapLoad}
         onMapClick={_.noop}
-        markers={markers}
+        markers={this.getMarkers()}
         onMarkerRightClick={_.noop}
+        bounds={this.getBounds()}
       />
     )
   }

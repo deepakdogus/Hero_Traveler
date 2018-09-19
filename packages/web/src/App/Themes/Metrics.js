165f8c6016ea7d9b43f6f137bacd7ebfccb6ece0
mbthiery <mbthiery@gmail.com>
2018-09-19 10:41:18 -0400

    added sizes to theme provider

--- packages/web/src/App/Themes/Metrics.js
+++ packages/web/src/App/Themes/Metrics.js
@@ -2,17 +2,18 @@
 const height = window.innerHeight
 const width = window.innerWidth
 
-const webMetrics = {
-  screenWidth: width,
-  screenHeight: height,
-  navBarHeight: 79,
-}
-
 export const sizes = {
   desktopLarge: 1200,
   desktop: 992,
   tablet: 768,
   phone: 376
+}
+
+const webMetrics = {
+  screenWidth: width,
+  screenHeight: height,
+  navBarHeight: 79,
+  sizes,
 }
 
 export default webMetrics

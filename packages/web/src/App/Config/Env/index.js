5e4b7a813757dd6294b6d6a45a4741a94391554e
mbthiery <mbthiery@gmail.com>
2018-09-20 15:07:04 -0400

    refactored env

--- packages/web/src/App/Config/Env/index.js
+++ packages/web/src/App/Config/Env/index.js
@@ -1,4 +1,19 @@
 // import devSettings from './dev'
 import prodSettings from './prod'
 
+export {
+  API_URL: process.env.API_URL,
+  cdnBaseUrl: process.env.CDN_BASE_URL,
+  cloudName: process.env.CLOUD_NAME,
+  imagePreset: process.env.IMAGE_PRESET,
+  videoPreset: process.env.VIDEO_PRESET,
+
+  SEARCH_APP_NAME: process.env.SEARCH_APP_NAME,
+  SEARCH_API_KEY: process.env.SEARCH_API_KEY,
+  SEARCH_STORY_INDEX: process.env.SEARCH_STORY_INDEX,
+  SEARCH_USER_INDEX: process.env.SEARCH_USER_INDEX,
+  SEARCH_CATEGORIES_INDEX: process.env.SEARCH_CATEGORIES_INDEX,
+  SEARCH_HASHTAGS_INDEX: process.env.SEARCH_HASHTAGS_INDEX,
+}
+
 export default process.env.NODE_ENV === 'development' ? prodSettings : prodSettings

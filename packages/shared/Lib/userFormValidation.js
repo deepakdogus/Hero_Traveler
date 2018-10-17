8af449e32d4ddb716e8fc98adc7c20a8564a56f4
mbthiery <mbthiery@gmail.com>
2018-10-17 17:08:54 -0400

    implements user profile edit with new specs

--- packages/shared/Lib/userFormValidation.js
+++ packages/shared/Lib/userFormValidation.js
@@ -11,6 +11,7 @@ export const FieldConstraints = {
   USERNAME_MIN_LENGTH: 5,
   USERNAME_MAX_LENGTH: 20,
   USERNAME_REGEX: /(?=^.{5,20}$)^[a-zA-Z][a-zA-Z0-9]*[._-]?[a-zA-Z0-9]+$/,
+  FULLNAME_MAX_LENGTH: 255,
   EMAIL_REGEX: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 }
 

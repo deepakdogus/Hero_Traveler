e94cbebc859c4e0355ebfdb926dd893fb1b05053
jamescalhoun <james.calhoun@gmail.com>
2018-10-23 18:00:04 -0400

    web/ui/explore-grid columns
    HT-797: https://rehash.atlassian.net/browse/HT-797

--- packages/web/src/App/Components/ExploreGrid.js
+++ packages/web/src/App/Components/ExploreGrid.js
@@ -69,7 +69,7 @@ class Tile extends React.Component {
   render(){
     const {category, isSelected} = this.props
     return (
-      <Col xs={4} md={3} lg={2} >
+      <Col xs={4} md={4} lg={3} >
         <Wrapper onClick={this._onClickTile}>
           <CategoryTile
             imageSource={getImageUrl(category.image, 'optimized', {width: 400, height: 400})}

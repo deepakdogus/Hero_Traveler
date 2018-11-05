97a95dbfc3761af85b103db905833654147e9096
mbthiery <mbthiery@gmail.com>
2018-11-05 13:56:40 -0500

    fixes facebook login bug

--- packages/HeroTravelerMobile/ios/HeroTravelerMobile/AppDelegate.m
+++ packages/HeroTravelerMobile/ios/HeroTravelerMobile/AppDelegate.m
@@ -99,13 +99,6 @@ fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
   return YES;
 }
 
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
-    if (![RNBranch.branch application:app openURL:url options:options]) {
-        // do other deep link routing for the Facebook SDK, Pinterest SDK, etc
-    }
-    return YES;
-}
-
 - (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *restorableObjects))restorationHandler {
     return [RNBranch continueUserActivity:userActivity];
 }

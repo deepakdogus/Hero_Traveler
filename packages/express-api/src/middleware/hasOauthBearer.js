a7ae2f6f10df7ca09e97cda4a75cecfd318e3bc8
mbthiery <mbthiery@gmail.com>
2018-11-13 18:03:17 -0500

    hasOauthBearer works properly again

--- packages/express-api/src/middleware/hasOauthBearer.js
+++ packages/express-api/src/middleware/hasOauthBearer.js
@@ -1,7 +1,7 @@
 import passport from 'passport'
 
 export default function hasOauthBearer(req, res, next) {
-  if (req.err && !req.user) {
+  if (req.err || !req.user) {
     return next(new Error('Unauthorized'))
   }
   else next()

43aa6dcfcf1956c253ad7efc82258b310ec87927
mbthiery <mbthiery@gmail.com>
2018-11-14 20:14:59 -0500

    fixing CORS for app

--- packages/express-api/src/index.js
+++ packages/express-api/src/index.js
@@ -32,7 +32,7 @@ const whitelist = [
 
 app.use(cors({
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))

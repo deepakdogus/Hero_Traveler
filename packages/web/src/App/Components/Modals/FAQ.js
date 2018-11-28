231ced5ce64a222b90349992a37b7e44355f9bac
user <youanswer@gmail.com>
2018-11-28 22:12:59 +0800

    Updates faq text and avoids html escape in answers text

--- packages/web/src/App/Components/Modals/FAQ.js
+++ packages/web/src/App/Components/Modals/FAQ.js
@@ -15,7 +15,9 @@ const Question = styled.p`
   letter-spacing: .2px;
 `
 
-const Answer = styled.p`
+const DangerousP = ({ html }) => (<p dangerouslySetInnerHTML={{ __html: html }} />)
+
+const Answer = styled(DangerousP)`
   font-family: ${props => props.theme.Fonts.type.base};
   font-weight: 400;
   font-size: 16px;
@@ -32,7 +34,7 @@ const FAQs = [
 {q: 'How do I import a video?', a: 'After selecting the video option, simply tap at the bottom of the screen on “library” to bring up your videos from your camera roll.'},
 {q: 'Does HERO Traveler support 4K Video?', a: 'While you cannot take 4K videos within the app itself just yet, you can still import 4K videos.'},
 {q: 'How do I become a HERO Traveler Contributor?', a: 'On the HERO Traveler App, simply tap the “plus” sign at the bottom of any page to bring up the option to create a story or start filming a video.'},
-{q: 'How do I post a story?', a: 'Contributors are the cream of the crop, representing our most passionate users. To be labeled with the “Contributor” star on your profile, you must first publish 200 stories or more. To learn about other eligibility requirements, click here/ send us an email at contributors@herotraveler.com'},
+{q: 'How do I post a story?', a: 'Contributors are the cream of the crop, representing our most passionate users. To be labeled with the “Contributor” star on your profile, you must first publish 200 stories or more. To learn about other eligibility requirements, click here/send us an email at contributors@herotraveler.com &lt;<a href="mailto:contributors@herotraveler.com">contributors@herotraveler.com</a>&gt;'},
 ]
 
 export default class FAQ extends Component {
@@ -41,7 +43,7 @@ export default class FAQ extends Component {
       return (
         <Container key={index}>
           <Question>{FAQ.q}</Question>
-          <Answer>{FAQ.a}</Answer>
+          <Answer html={FAQ.a} />
         </Container>
       )
     })

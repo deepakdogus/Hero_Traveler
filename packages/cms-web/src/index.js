d8e2c7b2edeff2fea1dae28f4989f70a537e2585
user <youanswer@gmail.com>
2018-12-21 00:47:37 +0800

    Upd prod environment for cms-web: remove reactotron

--- packages/cms-web/src/index.js
+++ packages/cms-web/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './App/Config/ReactotronConfig'
+// import './App/Config/ReactotronConfig'
 import './index.css'
 import './animations.css'
 import Root from './App/Containers/AppRoot'

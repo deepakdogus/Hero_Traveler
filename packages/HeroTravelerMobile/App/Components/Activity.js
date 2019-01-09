8abe132619fdcfaa9915e716880e8c95fc06efbe
allenhj <allendotjohnson@gmail.com>
2019-01-09 13:24:08 -0500

    make avatar, username, and feedItem title nav links

--- packages/HeroTravelerMobile/App/Components/Activity.js
+++ packages/HeroTravelerMobile/App/Components/Activity.js
@@ -1,6 +1,7 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { Actions as NavActions } from 'react-native-router-flux'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import moment from 'moment'
 
@@ -8,19 +9,50 @@ import { Colors, Metrics } from '../Shared/Themes/'
 import Avatar from '../Components/Avatar'
 import getImageUrl from '../Shared/Lib/getImageUrl'
 import {
-  getDescription,
+  getDescriptionText,
   getContent,
   getAvatar,
   getUsername,
+  getUserId,
+  ActivityTypes,
+  getFeedItemTitle,
 } from '../Shared/Lib/NotificationHelpers'
+import { displayLocationPreview } from '../Shared/Lib/locationHelpers'
 
 export default class Activity extends Component {
   static propTypes = {
-  onPress: PropTypes.func,
-  activity: PropTypes.object,
-}
+    onPress: PropTypes.func,
+    activity: PropTypes.object,
+    markSeen: PropTypes.func,
+  }
 
-  render () {
+  _markSeen = () => {
+    const {
+      activity: { seen, id },
+      markSeen,
+    } = this.props
+    if (!seen) markSeen(id)
+  }
+
+  _navToUser = () => {
+    const { activity } = this.props
+    NavActions.readOnlyProfile({ userId: getUserId(activity) })
+  }
+
+  _navToReadingScreen = () => {
+    const { activity } = this.props
+    const type
+      = activity.kind === ActivityTypes.like
+      || activity.kind === ActivityTypes.comment
+        ? 'story'
+        : 'guide'
+    NavActions[type]({
+      [`${type}Id`]: activity[type]._id,
+      title: displayLocationPreview(activity[type].locationInfo),
+    })
+  }
+
+  render() {
     let { activity } = this.props
     const content = getContent(activity)
 
@@ -29,53 +61,44 @@ export default class Activity extends Component {
 
     return (
       <View style={styles.root}>
-        <TouchableOpacity
-          onPress={this._onPress}
-        >
-          <View style={styles.innerButton}>
+        <TouchableOpacity onPress={this._markSeen}>
+          <View style={styles.container}>
+            <TouchableOpacity onPress={this._navToUser}>
               <Avatar
-                style={styles.avatar}
-                avatarUrl={
-                  avatar
-                  ? getImageUrl(avatar, 'avatar')
-                  : null
-                }
+                avatarUrl={avatar ? getImageUrl(avatar, 'avatar') : null}
               />
+            </TouchableOpacity>
             <View style={styles.middle}>
               <Text style={styles.description}>
-                <Text style={styles.actionUserText}>
-                  {username || 'A user'}
+                <Text style={styles.actionText} onPress={this._navToUser}>
+                  {`${username} ` || 'A user '}
                 </Text>
-                <Text> {getDescription(activity)}</Text>
+                <Text>{getDescriptionText(activity)}</Text>
+                <Text
+                  style={styles.actionText}
+                  onPress={this._navToReadingScreen}
+                >
+                  {getFeedItemTitle(activity)}
+                </Text>
               </Text>
-              {!!content &&
+              {!!content && (
                 <View style={styles.content}>
-                  <Text
-                    numberOfLines={2}
-                    style={styles.contentText}
-                  >
+                  <Text numberOfLines={2} style={styles.contentText}>
                     {content}
                   </Text>
                 </View>
-              }
+              )}
               <Text style={styles.dateText}>
                 {moment(activity.createdAt).fromNow()}
               </Text>
             </View>
-            {!activity.seen &&
-              <Icon name='circle' size={10} color={Colors.redLight}/>
-            }
+            {!activity.seen && (
+              <Icon name="circle" size={10} color={Colors.redLight} />
+            )}
           </View>
         </TouchableOpacity>
       </View>
     )
-  }
-
-  _onPress = () => {
-    const {activity, onPress} = this.props
-    if (onPress) {
-      onPress(activity.id, activity.seen)
-    }
   }
 }
 
@@ -85,10 +108,10 @@ const styles = StyleSheet.create({
     borderBottomColor: '#bdbdbd',
     borderBottomWidth: 1,
   },
-  innerButton: {
+  container: {
     flexDirection: 'row',
-    paddingVertical: Metrics.baseMargin,
-    paddingHorizontal: Metrics.doubleBaseMargin,
+    paddingVertical: 15,
+    paddingHorizontal: 15,
   },
   middle: {
     width: Metrics.screenWidth - 40 * 1.5 - Metrics.doubleBaseMargin * 2 - 50,
@@ -111,10 +134,11 @@ const styles = StyleSheet.create({
     fontWeight: '300',
     letterSpacing: 0.7,
   },
-  avatar: {
-    marginHorizontal: Metrics.baseMargin,
+  descriptionClickContainer: {
+    height: '100%',
+    width: '100%',
   },
-  actionUserText: {
+  actionText: {
     fontWeight: '600',
     color: Colors.background,
   },

7c2ebf1e0946f6d5e70c67c2f9b845efa16fbaa0
allenhj <allendotjohnson@gmail.com>
2019-01-23 16:12:23 -0500

    NofificationBadge refactor, styles for all nubmer classes

--- packages/HeroTravelerMobile/App/Components/NotificationBadge.js
+++ packages/HeroTravelerMobile/App/Components/NotificationBadge.js
@@ -4,25 +4,44 @@ import Colors from '../Shared/Themes/Colors'
 
 const styles = StyleSheet.create({
   wrapper: {
+    alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: Colors.redLight,
+    borderRadius: 6,
     position: 'absolute',
-    top: -10,
-    right: -10,
-    borderRadius: 8,
-    paddingHorizontal: 3,
-    paddingVertical: 1,
+    top: 5,
+    right: 2.5,
+    height: 12,
+    width: 12,
+  },
+  doubleDigits: {
+    width: 14,
+    right: 0,
+  },
+  tripleDigits: {
+    width: 18,
+    right: -2.5,
+  },
+  maximum: {
+    width: 22,
+    right: -5,
   },
   text: {
     color: Colors.snow,
     fontSize: 9,
-    marginRight: 1,
   },
 })
 
 export default function NotificationBadge({count, style}) {
   return (
-    <View style={[styles.wrapper, style]}>
-      <Text style={styles.text}>{count}</Text>
+    <View style={[
+      styles.wrapper,
+      count > 9 && styles.doubleDigits,
+      count > 99 && styles.tripleDigits,
+      count > 999 && styles.maximum,
+      style,
+    ]}>
+      <Text style={styles.text}>{count > 999 ? '999+' : count}</Text>
     </View>
   )
 }

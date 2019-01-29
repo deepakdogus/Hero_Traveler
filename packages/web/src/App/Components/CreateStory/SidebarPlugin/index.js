591f31fd2e44cbb62cdec7b1ed58125590ac5a31
allenhj <allendotjohnson@gmail.com>
2019-01-29 17:21:57 -0500

    draftjs sidebar button always visible

---
+++ packages/web/src/App/Components/CreateStory/SidebarPlugin/index.js
@@ -0,0 +1,43 @@
+import decorateComponentWithProps from 'decorate-component-with-props'
+import createStore from './utils/createStore'
+import Toolbar from './Toolbar'
+
+/*
+ * Forked from draft-js-side-toolbar-plugin
+ * Created by Benjamin Kniffler (benjamin@kniffler.com)
+ *
+ * DO NOT REMOVE LICENSE
+ *
+ * Original: https://github.com/draft-js-plugins/draft-js-plugins/tree/master/draft-js-side-toolbar-plugin
+ *
+ */
+export default (config = {}) => {
+  const defaultPostion = 'left'
+
+  // const defaultTheme = { buttonStyles }
+
+  const store = createStore({
+    isVisible: true,
+  })
+
+  const { position = defaultPostion } = config
+
+  const toolbarProps = {
+    store,
+    position,
+  }
+
+  return {
+    initialize: ({ setEditorState, getEditorState, getEditorRef }) => {
+      store.updateItem('getEditorState', getEditorState)
+      store.updateItem('setEditorState', setEditorState)
+      store.updateItem('getEditorRef', getEditorRef)
+    },
+    // Re-Render the toolbar on every change
+    onChange: editorState => {
+      store.updateItem('editorState', editorState)
+      return editorState
+    },
+    SideToolbar: decorateComponentWithProps(Toolbar, toolbarProps),
+  }
+}

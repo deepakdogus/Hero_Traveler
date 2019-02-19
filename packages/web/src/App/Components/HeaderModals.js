1bf0fe25c8350cec5f28bf5106624738b189ed3b
Yin Wang <dongfangworld@gmail.com>
2019-02-19 09:50:40 +0800

    add password reset success modal to web

--- packages/web/src/App/Components/HeaderModals.js
+++ packages/web/src/App/Components/HeaderModals.js
@@ -11,6 +11,7 @@ import SaveEdits from './Modals/HeaderModals/SaveEdits'
 import ExistingUpdateWarning from './Modals/HeaderModals/ExistingUpdateWarning'
 import ResetPasswordRequest from './Modals/HeaderModals/ResetPasswordRequest'
 import ResetPasswordAttempt from './Modals/HeaderModals/ResetPasswordAttempt'
+import ResetPasswordSuccess from './Modals/HeaderModals/ResetPasswordSuccess'
 import ChangeTempUsername from './Modals/ChangeTempUsername'
 import Contributor from './Modals/HeaderModals/Contributor'
 import FlagStory from './Modals/FlagStory'
@@ -156,6 +157,13 @@ export default class HeaderModals extends React.Component {
           <ResetPasswordAttempt />
         </Modal>
         <Modal
+          isOpen={globalModalThatIsOpen === 'resetPasswordSuccess'}
+          contentLabel="Reset Password Success Modal"
+          style={customModalStyles}
+        >
+          <ResetPasswordSuccess />
+        </Modal>
+        <Modal
           isOpen={globalModalThatIsOpen === 'contributor'}
           contentLabel='Contributor'
           onRequestClose={closeModal}
@@ -297,7 +305,10 @@ export default class HeaderModals extends React.Component {
           contentLabel='Inbox'
           onRequestClose={closeModal}
         >
-          <Inbox closeModal={closeModal} profile={currentUserProfile}/>
+          <Inbox
+            closeModal={closeModal}
+            profile={currentUserProfile}
+          />
         </RightModal>
       </Container>
     )
---
+++ packages/web/src/App/Components/Modals/HeaderModals/ResetPasswordSuccess.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { push } from 'react-router-redux'
+import { connect } from 'react-redux'
+
+import UXActions from '../../../Redux/UXRedux'
+import RoundedButton from '../../RoundedButton'
+import { Row } from '../../FlexboxGrid'
+import { Container, Title } from '../../Modals/Shared'
+
+class ResetPasswordSuccess extends React.Component {
+  static propTypes = {
+    path: PropTypes.string,
+    openGlobalModal: PropTypes.func,
+    reroute: PropTypes.func,
+  };
+
+  componentDidUpdate = prevProps => {
+    const { reroute, openGlobalModal, path } = this.props
+    reroute(path)
+    openGlobalModal('login')
+  };
+
+  _handleOkay = e => {
+    e.preventDefault()
+
+    const { reroute, openGlobalModal, path } = this.props
+    reroute(path)
+    openGlobalModal('login')
+  };
+
+  render() {
+    return (
+      <Container>
+        <Title>Your password has been updated.</Title>
+
+        <Row center="xs">
+          <RoundedButton
+            onClick={this._handleOkay}
+            text="OK"
+            width="100%"
+            margin="none"
+            height="39px"
+          />
+        </Row>
+      </Container>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    path: state.ux.params.path,
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    reroute: path => dispatch(push(path)),
+    openGlobalModal: (modalName, params) => dispatch(UXActions.openGlobalModal(modalName, params)),
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(ResetPasswordSuccess)

4a6b021e650d4ddda237d95753e8f728879c42aa
mbthiery <mbthiery@gmail.com>
2019-03-23 15:25:05 -0400

    fixes redux-logger + redux-persist + connects sagas

--- packages/HeroTravelerMobile/App/Config/ReduxPersist.js
+++ packages/HeroTravelerMobile/App/Config/ReduxPersist.js
@@ -1,16 +1,50 @@
+import _ from 'lodash'
+import AsyncStorage from '@react-native-community/async-storage'
+import { createTransform, PersistConfig } from 'redux-persist'
+import Immutable from 'seamless-immutable'
 import immutablePersistenceTransform from '../Shared/Services/ImmutablePersistenceTransform'
-import { AsyncStorage } from '@react-native-community/async-storage'
 
-const REDUX_PERSIST = {
-  active: true,
-  reducerVersion: '12',
-  storeConfig: {
-    storage: AsyncStorage,
-    // blacklist: ['login', 'search', 'feed'], // reducer keys that you do NOT want stored to persistence here
-    whitelist: ['session', 'entities', 'history', 'pendingUpdates'], // Optionally, just specify the keys you DO want stored to
-    // persistence. An empty array means 'don't store any reducers' -> infinitered/ignite#409
-    transforms: [immutablePersistenceTransform],
+// const REDUX_PERSIST = {
+//   active: true,
+//   reducerVersion: '12',
+//   storeConfig: {
+//     storage: AsyncStorage,
+//     // blacklist: ['login', 'search', 'feed'], // reducer keys that you do NOT want stored to persistence here
+//     whitelist: ['session', 'entities', 'history', 'pendingUpdates'], // Optionally, just specify the keys you DO want stored to
+//     // persistence. An empty array means 'don't store any reducers' -> infinitered/ignite#409
+//     transforms: [immutablePersistenceTransform],
+//   },
+// }
+
+const transform = createTransform(
+  // transform state on its way to being serialized and persisted.
+  // inboundState should be an immutable object
+  (inboundState, key) => {
+    if (_.has(inboundState, 'asMutable')) {
+      return inboundState.asMutable({ deep: true })
+    }
+    return inboundState
+  },
+  // transform state being rehydrated
+  // outboundState should be a raw object
+  (outboundState, key) => {
+    return Immutable(outboundState)
+  },
+)
+
+export const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  version: 1,
+  transforms: [transform],
+  migrate: state => {
+    if (_.get(state, '_persist.version') === version) {
+      return Promise.resolve(state)
+    }
+    return Promise.resolve({
+      _persist: state._persist,
+    })
   },
 }
 
-export default REDUX_PERSIST
+// export default REDUX_PERSIST
--- packages/shared/Redux/CreateStore.js
+++ packages/shared/Redux/CreateStore.js
@@ -3,61 +3,65 @@ import {
   applyMiddleware,
   compose,
 } from 'redux'
-import { autoRehydrate } from 'redux-persist'
+import { autoRehydrate, persistReducer } from 'redux-persist'
 import Config from '../../Config/DebugConfig'
 import createSagaMiddleware from 'redux-saga'
 import RehydrationServices from '../Services/RehydrationServices'
-import CreateLogger from 'redux-logger'
-import ReduxPersist from '../../Config/ReduxPersist'
+import { createLogger } from 'redux-logger'
+import ReduxPersist, { persistConfig } from '../../Config/ReduxPersist'
 import {middleware as routerMiddleware} from '../../Redux/Routes'
 
 // creates the store
 export default (rootReducer, rootSaga) => {
-  // /* ------------- Redux Configuration ------------- */
+  /* ------------- Redux Configuration ------------- */
 
   const middleware = []
   const enhancers = []
 
-  // /* ------------- Saga Middleware ------------- */
+  /* ------------- Saga Middleware ------------- */
 
   // const sagaMonitor = process.env.NODE_ENV === 'development' ? console.tron.createSagaMonitor() : null
-  // const sagaMiddleware = createSagaMiddleware({ sagaMonitor })
-  // middleware.push(sagaMiddleware)
+  const sagaMonitor = null
+  const sagaMiddleware = createSagaMiddleware({ sagaMonitor })
+  middleware.push(sagaMiddleware)
 
   if (Config.reduxLogging) {
-    middleware.push(CreateLogger({collapsed: true}))
+    middleware.push(createLogger({collapsed: true}))
   }
 
-  // /* ------------- Navigation Middleware ------------- */
+  /* ------------- Navigation Middleware ------------- */
   // do we use this - look at history?
   if (routerMiddleware) {
     middleware.push(routerMiddleware)
   }
 
-  // /* ------------- Assemble Middleware ------------- */
+  /* ------------- Assemble Middleware ------------- */
 
   enhancers.push(applyMiddleware(...middleware))
 
-  // /* ------------- AutoRehydrate Enhancer ------------- */
+  /* ------------- AutoRehydrate Enhancer ------------- */
 
-  // // add the autoRehydrate enhancer
-  if (ReduxPersist.active) {
+  // add the autoRehydrate enhancer
+  if (!persistConfig && ReduxPersist.active) {
     enhancers.push(autoRehydrate())
   }
 
-  // // if Reactotron is enabled (default for process.env.NODE_ENV === 'development'), we'll create the store through Reactotron
+  // if Reactotron is enabled (default for process.env.NODE_ENV === 'development'), we'll create the store through Reactotron
   // const createAppropriateStore = Config.useReactotron ? console.tron.createStore : createStore
   // const store = createAppropriateStore(rootReducer, compose(...enhancers))
-  const store = createStore(rootReducer, compose(...enhancers))
+  let reducerToUse = rootReducer
+  if (persistConfig) {
+    reducerToUse = persistReducer(persistConfig, rootReducer)
+  }
+  const store = createStore(reducerToUse, compose(...enhancers))
 
-  // // configure persistStore and check reducer version number
-  if (ReduxPersist.active) {
-    console.log("rehydrating")
+  // configure persistStore and check reducer version number
+  if (!persistConfig && ReduxPersist.active) {
     RehydrationServices.updateReducers(store)
   }
 
-  // // kick off root saga
-  // sagaMiddleware.run(rootSaga)
+  // kick off root saga
+  sagaMiddleware.run(rootSaga)
 
   return store
 }

8cafb100a6b51fe905cb54fb0a730416f6925a12
allenhj <allendotjohnson@gmail.com>
2019-04-10 15:56:20 -0400

    display sublocalities and hide countries

--- packages/web/src/App/Containers/Signup/SignupAdditionalInfo.js
+++ packages/web/src/App/Containers/Signup/SignupAdditionalInfo.js
@@ -259,6 +259,7 @@ class SignupAdditionalInfo extends Component {
           const AutocompleteContainer = suggestion.active
             ? ActiveAutocompleteItemContainer
             : InactiveAutocompleteItemContainer
+          if (suggestion.types.includes('country')) return null
           return (
             <AutocompleteContainer
               key={suggestion.placeId}
@@ -316,7 +317,7 @@ class SignupAdditionalInfo extends Component {
               <Section>
                 <GoogleLocator
                   value={address}
-                  searchOptions={{ types: ['(cities)'] }}
+                  searchOptions={{ types: ['(regions)'] }}
                   onChange={this.handleHometownChange}
                   onSelect={this.handleHometownSelect}
                   renderChildren={this.renderHometownInput}

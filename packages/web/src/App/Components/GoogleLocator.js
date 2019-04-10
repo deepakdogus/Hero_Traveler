29a8fc1480a091c88ea9dd6e4be478f1356efe76
allenhj <allendotjohnson@gmail.com>
2019-04-10 13:58:28 -0400

    turn off bias for user's location in google locator

--- packages/web/src/App/Components/GoogleLocator.js
+++ packages/web/src/App/Components/GoogleLocator.js
@@ -4,6 +4,14 @@ import PropTypes from 'prop-types'
 import PlacesAutocomplete from 'react-places-autocomplete'
 import ExtendedPlacesAutocomplete from './Extensions/ExtendedPlacesAutocomplete'
 
+/* global google */
+
+// bounds to specify entire earth should be searched, with no bias towards user's location
+const bounds = new google.maps.LatLngBounds(
+  new google.maps.LatLng(85, -180),
+  new google.maps.LatLng(-85, 180),
+)
+
 class GoogleLocator extends React.Component {
   static propTypes = {
     value: PropTypes.string,
@@ -30,7 +38,7 @@ class GoogleLocator extends React.Component {
         value={value}
         onChange={onChange}
         onSelect={onSelect}
-        searchOptions={searchOptions}
+        searchOptions={{bounds, ...searchOptions}}
       >
         {renderChildren}
       </GoogleLocator>

b15ec7ae3a1b4b25ad265a47cb8fd0de0ba42112
allenhj <allendotjohnson@gmail.com>
2019-04-10 13:51:19 -0400

    replace location formatter with placeId lookup

--- packages/shared/Lib/formatLocation.js
+++ packages/shared/Lib/formatLocation.js
@@ -3,34 +3,47 @@ import _ from 'lodash'
 export default function formatLocation(place) {
   return {
     name: place.name,
-    locality: _.get(place, 'addressComponents.sublocality_level_1') || _.get(place, 'addressComponents.locality'),
+    locality:
+      _.get(place, 'addressComponents.sublocality_level_1') ||
+      _.get(place, 'addressComponents.locality'),
     state: _.get(place, 'addressComponents.administrative_area_level_1'),
     country: _.get(place, 'addressComponents.country'),
     latitude: place.latitude,
-    longitude: place.longitude,
+    longitude: place.longitude
   }
 }
 
-export async function formatLocationWeb(place, geocodeByAddress, getLatLng) {
-  const geocode = await geocodeByAddress(place)
-  const result = await extractWeb(geocode[0], getLatLng)
-  return {
-    name: place.split(',')[0],
-    locality: result.sublocality_level_1 || result.locality,
-    state: result.administrative_area_level_1,
-    formattedAdress: geocode[0].formatted_address,
-    country: result.country,
-    latitude: result.latitude,
-    longitude: result.longitude,
+export async function formatLocationWeb(
+  description,
+  placeId,
+  geocodeByPlaceId,
+  getLatLng
+) {
+  try {
+    const geocode = await geocodeByPlaceId(placeId)
+    const result = await extractWeb(geocode[0], getLatLng)
+    return {
+      name: description.split(',')[0],
+      locality: result.sublocality_level_1 || result.locality,
+      state: result.administrative_area_level_1,
+      formattedAdress: geocode[0].formatted_address,
+      country: result.country,
+      latitude: result.latitude,
+      longitude: result.longitude
+    }
+  } catch (e) {
+    console.error(e)
   }
 }
 
 async function extractWeb(place, getLatLng) {
   const addressComponents = {}
   place.address_components.forEach(component => {
-    if (component.types) component.types.forEach(type => {
-      if (type !== 'political') addressComponents[type] = component.long_name
-    })
+    if (component.types) {
+      component.types.forEach(type => {
+        if (type !== 'political') addressComponents[type] = component.long_name
+      })
+    }
   })
   let latLng = await getLatLng(place)
   addressComponents.latitude = latLng.lat
--- packages/web/src/App/Containers/Search.js
+++ packages/web/src/App/Containers/Search.js
@@ -20,7 +20,7 @@ import { Row } from '../Components/FlexboxGrid'
 // search
 import algoliasearchModule from 'algoliasearch'
 import algoliaSearchHelper from 'algoliasearch-helper'
-import { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
+import { geocodeByPlaceId, getLatLng } from 'react-places-autocomplete'
 import { formatLocationWeb } from '../Shared/Lib/formatLocation'
 
 const MAX_RESULTS = 10
@@ -250,7 +250,7 @@ class Search extends Component {
       })
       return this.helper.setQuery(textValue).search()
     }
- else {
+    else {
       this.setState({ activeTab, algoliaResults: {} })
     }
   }
@@ -276,32 +276,45 @@ class Search extends Component {
     this.props.reroute(`/profile/${userId}/view`)
   }
 
-  navToLocationResults = async ({ id, description, secondaryText }) => {
+  navToLocationResults = async ({ description, placeId, secondaryText }) => {
     const { reroute, addRecentSearch } = this.props
-    let { name: title, country, latitude: lat, longitude: lng } = await formatLocationWeb(
-      description,
-      geocodeByAddress,
-      getLatLng,
-    )
-
-    if (lat && lng) {
-      addRecentSearch({
-        searchType: 'places',
-        searchText: this.state.inputText,
-        contentType: 'location',
-        id,
-        title,
-        lat,
-        lng,
+    try {
+      let {
+        name: title,
+        country,
+        latitude: lat,
+        longitude: lng,
+      } = await formatLocationWeb(
         description,
-        secondaryText,
-      })
-      reroute({
-        pathname: `/results/${country}/${lat}/${lng}`,
-        search: `?t=${title}${
-          hasSecondaryText(secondaryText) ? `, ${formatSecondaryText(secondaryText)}` : ''
-        }`,
-      })
+        placeId,
+        geocodeByPlaceId,
+        getLatLng,
+      )
+
+      if (lat && lng) {
+        addRecentSearch({
+          searchType: 'places',
+          searchText: this.state.inputText,
+          contentType: 'location',
+          id: placeId,
+          title,
+          lat,
+          lng,
+          description,
+          secondaryText,
+        })
+        reroute({
+          pathname: `/results/${country}/${lat}/${lng}`,
+          search: `?t=${title}${
+            hasSecondaryText(secondaryText)
+              ? `, ${formatSecondaryText(secondaryText)}`
+              : ''
+          }`,
+        })
+      }
+    }
+    catch (e) {
+      console.error(e)
     }
   }
 
@@ -321,7 +334,7 @@ class Search extends Component {
     const {
       searchHistory: { places },
     } = this.props
-    if (!inputText)
+    if (!inputText) {
       return (
         <SearchAutocompleteList
           label="RECENT SEARCHES"
@@ -329,9 +342,11 @@ class Search extends Component {
           navigate={this.navToLocationResults}
         />
       )
+    }
 
     const formattedLocations = suggestions.map(suggestion => ({
       id: suggestion.id,
+      placeId: suggestion.placeId,
       title: suggestion.formattedSuggestion.mainText,
       secondaryText: suggestion.formattedSuggestion.secondaryText,
       description: suggestion.description,
@@ -397,18 +412,18 @@ class Search extends Component {
   }
 
   renderChildren = ({ getInputProps, suggestions }) => (
-    <Container>
-      <HeaderInputContainer between="xs">
-        <HeaderInput
-          {...getInputProps({
-            placeholder: 'Type to search',
-          })}
-        />
-        <Text onClick={this.resetSearchText}>{'Cancel'}</Text>
-      </HeaderInputContainer>
-      {this.renderTab(suggestions)}
-    </Container>
-  )
+      <Container>
+        <HeaderInputContainer between="xs">
+          <HeaderInput
+            {...getInputProps({
+              placeholder: 'Type to search',
+            })}
+          />
+          <Text onClick={this.resetSearchText}>{'Cancel'}</Text>
+        </HeaderInputContainer>
+        {this.renderTab(suggestions)}
+      </Container>
+    )
 
   /* The `react-places-autocomplete` package rerenders the child based on the search
    * input. We pass all search components through GoogleLocator so we don't have to
@@ -441,17 +456,19 @@ const mapStateToProps = state => {
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
     followUser: (sessionUserId, userIdToFollow) =>
-      dispatch(runIfAuthed(
-        sessionUserId,
-        UserActions.followUser,
-        [sessionUserId, userIdToFollow],
-      )),
+      dispatch(
+        runIfAuthed(sessionUserId, UserActions.followUser, [
+          sessionUserId,
+          userIdToFollow,
+        ]),
+      ),
     unfollowUser: (sessionUserId, userIdToUnfollow) =>
-      dispatch(runIfAuthed(
-        sessionUserId,
-        UserActions.unfollowUser,
-        [sessionUserId, userIdToUnfollow],
-      )),
+      dispatch(
+        runIfAuthed(sessionUserId, UserActions.unfollowUser, [
+          sessionUserId,
+          userIdToUnfollow,
+        ]),
+      ),
     loadUserFollowing: sessionUserID =>
       dispatch(UserActions.loadUserFollowing(sessionUserID)),
     reroute: path => dispatch(push(path)),

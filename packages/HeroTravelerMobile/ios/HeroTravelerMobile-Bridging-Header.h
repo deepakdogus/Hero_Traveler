1a62583a802cb222e6bd9e55a154899abfcd2cb2
mbthiery <mbthiery@gmail.com>
2019-04-15 17:39:57 -0400

    adds react-native-video-processing

---
+++ packages/HeroTravelerMobile/ios/HeroTravelerMobile-Bridging-Header.h
@@ -0,0 +1,18 @@
+//
+//  HeroTravelerMobile-Bridging-Header.h
+//  HeroTravelerMobile
+//
+//  Created by Matthew Thiery on 4/15/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#ifndef HeroTravelerMobile_Bridging_Header_h
+#define HeroTravelerMobile_Bridging_Header_h
+
+#import <React/RCTRootView.h>
+#import <React/RCTView.h>
+#import <React/RCTBridgeModule.h>
+#import <react-native-branch/RNBranch.h>
+#import "RNVideoProcessing.h"
+
+#endif /* HeroTravelerMobile_Bridging_Header_h */

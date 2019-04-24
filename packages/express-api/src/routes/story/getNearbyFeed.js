68289112a3b146c6a699f848034ac83f752af8df
allenhj <allendotjohnson@gmail.com>
2019-04-24 10:50:34 -0400

    sort nearby feed by algolia order

--- packages/express-api/src/routes/story/getNearbyFeed.js
+++ packages/express-api/src/routes/story/getNearbyFeed.js
@@ -1,9 +1,17 @@
-import {Story} from '@hero/ht-core'
+import { Story } from '@hero/ht-core'
 
-export default function getNearbyFeed(req, res) {
+export default async function getNearbyFeed(req, res) {
   const nearbyStoryIds = JSON.parse(req.query.nearbyStoryIds)
   // @TODO: incorporate pages for infinite scroll if >100 nearby results desired
   // const page = parseInt(req.query.page, 10)
   // const perPage = parseInt(req.query.perPage, 10)
-  return Story.getStoriesById(nearbyStoryIds)
+
+  // ensure stories are returned in closest-to-farthest order (the way they were sent)
+  const sortByIdPosition = (a, b) =>
+    nearbyStoryIds.indexOf(a.id) > nearbyStoryIds.indexOf(b.id)
+  const nearbyStories = await Story.getStoriesById(nearbyStoryIds)
+  return {
+    ...nearbyStories,
+    feed: nearbyStories.feed.sort(sortByIdPosition),
+  }
 }

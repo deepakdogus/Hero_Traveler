5b532cf62d0fb6c01578bcc5866b73503aef40e2
mbthiery <mbthiery@gmail.com>
2019-04-29 10:17:08 -0400

    adds app icons + launch screen + removed some recompose

--- packages/HeroTravelerMobile/App/Containers/Settings_NotificationScreen.js
+++ packages/HeroTravelerMobile/App/Containers/Settings_NotificationScreen.js
@@ -6,7 +6,8 @@ import {
 } from 'react-native'
 import { connect } from 'react-redux'
 import _ from 'lodash'
-import {withHandlers} from 'recompose'
+
+import { Colors } from '../Shared/Themes'
 
 const notificationOpts = [
   ['New Follower', 'user_new_follower'],
@@ -17,59 +18,33 @@ const notificationOpts = [
 import styles from './Styles/SettingsScreenStyles'
 import UserActions from '../Shared/Redux/Entities/Users'
 
-const enhancedRow = withHandlers({
-  onValChange: props => () => {
-    props.onPress(props.value)
-  }
-})
+class Row extends React.Component {
+  onValueChange = (props) => this.props.onPress(this.props.value)
 
-const Row = enhancedRow(({text, selected, onValChange}) => {
-  return (
-    <View style={styles.rowWrapper}>
-      <View style={styles.row}>
-        <Text style={[styles.rowText, {flexGrow: 1, color: 'black'}]}>{text}</Text>
-        <Switch onTintColor="red" value={selected} onValueChange={onValChange} />
+  render() {
+    const { text, selected, onValueChange } = this.props
+    return (
+      <View style={styles.rowWrapper}>
+        <View style={styles.row}>
+          <Text style={[styles.rowText, styles.notificationText]}>{text}</Text>
+          <Switch
+            trackColor={{ true: Colors.redHighlights }}
+            value={selected}
+            onValueChange={this.onValueChange}
+          />
+        </View>
       </View>
-    </View>
-  )
-})
-
-const RowList = ({children}) => {
-  return (
-    <View style={styles.list}>
-      {children}
-    </View>
-  )
+    )
+  }
 }
 
-class NotificationsSettingsScreen extends React.Component {
 
+class NotificationsSettingsScreen extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       selectedTypes: this.props.user.notificationTypes
     }
-  }
-
-  render () {
-    return (
-      <View style={[styles.containerWithNavbar, styles.root]}>
-        <Text style={styles.settingsLabel}>Push Notifications</Text>
-        <RowList>
-          {_.map(notificationOpts, ([label, value]) => {
-            return (
-              <Row
-                key={value}
-                text={label}
-                value={value}
-                onPress={this._changeVal}
-                selected={_.includes(this.state.selectedTypes, value)}
-              />
-            )
-          })}
-        </RowList>
-      </View>
-    )
   }
 
   _changeVal = (val) => {
@@ -81,6 +56,27 @@ class NotificationsSettingsScreen extends React.Component {
     }
     this.setState({selectedTypes: newTypes})
     this.props.updateUser({notificationTypes: newTypes})
+  }
+
+  render () {
+    return (
+      <View style={[styles.containerWithNavbar, styles.root]}>
+        <Text style={styles.settingsLabel}>Push Notifications</Text>
+        <View style={styles.list}>
+          {_.map(notificationOpts, ([label, value]) => {
+            return (
+              <Row
+                key={value}
+                text={label}
+                value={value}
+                onPress={this._changeVal}
+                selected={_.includes(this.state.selectedTypes, value)}
+              />
+            )
+          })}
+        </View>
+      </View>
+    )
   }
 }
 

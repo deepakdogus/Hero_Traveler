3663d17a205a7e9cc09ee1994bf9892f4853c09b
allenhj <allendotjohnson@gmail.com>
2019-05-27 14:25:24 -0400

    refactor .env var setup

---
+++ packages/HeroTravelerMobile/App/Config/Env.example.js
@@ -0,0 +1,23 @@
+const devSettings = {
+  API_URL: '', // must be ngrok if testing on device
+  cdnBaseUrl: '',
+  cloudName: '',
+  imagePreset: '',
+  videoPreset: '',
+
+  SEARCH_APP_NAME: '',
+  SEARCH_API_KEY: '',
+  SEARCH_STORY_INDEX: '',
+  SEARCH_USER_INDEX: '',
+  SEARCH_CATEGORIES_INDEX: '',
+  SEARCH_HASHTAGS_INDEX: '',
+  SEARCH_GUIDE_INDEX: '',
+
+  GOOGLE_API_KEY: '-',
+}
+
+const prodSettings = {
+  // get from dev
+}
+
+export default devSettings

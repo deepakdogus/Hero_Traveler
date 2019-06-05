f91552af23e22e90928a1e11443a9a0124b9883b
allenhj <allendotjohnson@gmail.com>
2019-06-05 11:50:30 -0400

    add null check

--- packages/shared/Lib/channelAndCategoryRender.js
+++ packages/shared/Lib/channelAndCategoryRender.js
@@ -1,21 +1,27 @@
-const getCategoriesOrChannels = (selectedTab, channelsByID, users, categories, tabTypes) => {
-    const channels = []
-    if (selectedTab === tabTypes.channels){
-      for (let i = 0; i < channelsByID.length; i++){
-        if (users[channelsByID[i]]){
-          channels.push(users[channelsByID[i]])
-        }
+const getCategoriesOrChannels = (
+  selectedTab,
+  channelsByID,
+  users,
+  categories,
+  tabTypes
+) => {
+  const channels = []
+  if (selectedTab === tabTypes.channels && !!channels) {
+    for (let i = 0; i < channelsByID.length; i++) {
+      if (users[channelsByID[i]]) {
+        channels.push(users[channelsByID[i]])
       }
     }
+  }
 
-    switch(selectedTab){
-      case tabTypes.categories:
-        return categories
-      case tabTypes.channels:
-        return channels
-      default:
-        return []
-    }
+  switch (selectedTab) {
+    case tabTypes.categories:
+      return categories
+    case tabTypes.channels:
+      return channels
+    default:
+      return []
+  }
 }
 
 export default getCategoriesOrChannels

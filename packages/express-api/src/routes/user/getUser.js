81d1de4861afc4f6ad6f432d75fb0d4da90991cd
DanielChen1234 <dchen8492@yahoo.com>
2019-06-17 11:14:57 -0400

    options boolean now rendering true / false

--- packages/express-api/src/routes/user/getUser.js
+++ packages/express-api/src/routes/user/getUser.js
@@ -1,7 +1,8 @@
 import {User} from '@hero/ht-core'
+import { userInfo } from 'os';
 
 export default function getUser(req, res) {
   console.log(req.user.id, 'this is a user id in the express route')
   const userId = req.params.id
-  return User.get({_id: userId})
+  return User.get({_id: userId}, req.user.id === userId)
 }
--- packages/ht-core/src/user/getUser.js
+++ packages/ht-core/src/user/getUser.js
@@ -1,6 +1,7 @@
 import {User} from '../models'
 
-export default function getUser(query, options) {
+export default function getUser(query, options = false) {
+  console.log(options, 'options')
   return User.findOne(query)
              .populate('profile.avatar')
              .populate('profile.cover')

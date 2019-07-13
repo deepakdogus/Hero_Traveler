02fe7646773ee1d45f0ffd176ffffb46aa407923
allenhj <allendotjohnson@gmail.com>
2019-07-13 10:47:25 -0400

    restyle headline divider component

--- packages/web/src/App/Components/HeadlineDivider.js
+++ packages/web/src/App/Components/HeadlineDivider.js
@@ -17,43 +17,45 @@ const FeedText = styled(CenteredText)`
   font-weight: 600;
   font-size: 30px;
   letter-spacing: 0.6px;
-  padding: 30px 0 0 0;
   @media (max-width: ${sizes.tablet}px) {
-    padding: 20px;
     font-size: 18px;
   }
 `
 
 const SponsorText = styled(CenteredText)`
+  flex-basis: 49%;
+  flex-shrink: 0;
   color: ${Colors.bioGrey};
   font-family: ${props => props.theme.Fonts.type.montserrat};
   font-weight: 100;
   font-size: 10px;
   letter-spacing: 0.3px;
-  padding: 30px 0 5px;
-  @media (max-width: ${sizes.tablet}px) {
-    padding: 20px;
-    font-size: 8px;
-  }
-  width: 100px;
   text-align: right;
   justify-content: right;
-  margin-right: 5px;
+  align-self: center;
 `
 
 const Wrapper = styled.div``
 
 const SponsorWrapper = styled.div`
   display: flex;
-  margin: 20px 0 auto;
-  justify-content: center;
-
+  margin: 40px auto 20px;
+  justify-content: ${props => props.noImage ? 'center' : 'space-between'};
+  max-width: 300px;
+  @media (max-width: ${sizes.phone}px) {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 `
 
-const ImageAndTextWrapper = styled.div`
-  display: flex;
-  width: 310px;
-  justify-content: center;
+const StyledImage = styled.img`
+  flex-basis: 49%;
+  flex-shrink: 0;
+  height: 40px;
+  width: 100px;
+  object-fit: contain;
+  justify-content: right;
 `
 
 const StyledDivider = styled(HorizontalDivider)`
@@ -65,34 +67,21 @@ const StyledDivider = styled(HorizontalDivider)`
   }
 `
 
-export const Img = styled.img`
-  margin-left: 5px;
-  padding: 30px 0 5px;
-  height: 40px;
-  width: 100px;
-  object-fit: contain;
-  justify-content: right;
-`
-
 export default class HeadlineDivider extends Component {
   static propTypes = {
+    img: PropTypes.string,
     title: PropTypes.string,
   }
 
   render() {
     const { title, img } = this.props
+    const Text = img ? SponsorText : FeedText
     return (
       <Wrapper>
-        {!img ? (
-          <FeedText>{title}</FeedText>
-        ) : (
-          <SponsorWrapper>
-            <ImageAndTextWrapper>
-              <SponsorText>{'Sponsored by'}</SponsorText>
-              {<Img src={getImageUrl(img)} />}
-            </ImageAndTextWrapper>
-          </SponsorWrapper>
-        )}
+        <SponsorWrapper noImage={!img}>
+          <Text>{title || 'Sponsored by'}</Text>
+          {img && <StyledImage src={getImageUrl(img)} />}
+        </SponsorWrapper>
         <StyledDivider />
       </Wrapper>
     )

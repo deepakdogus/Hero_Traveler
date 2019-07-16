06de208673bff8f94516bcf456c9a219afae9e2b
DanielChen1234 <dchen8492@yahoo.com>
2019-07-16 13:44:19 -0400

    changed express and ht-core algo

--- packages/express-api/src/routes/user/getUser.js
+++ packages/express-api/src/routes/user/getUser.js
@@ -2,7 +2,7 @@ import {User} from '@hero/ht-core'
 import { userInfo } from 'os';
 
 export default function getUser(req, res) {
-  const userProfileId = req.user && req.user.id
+  const userId = req.user && req.user.id
   const userParamsId = req.params.id
-  return User.get({_id: userParamsId},  userProfileId === userParamsId)
+  return User.get({_id: userParamsId},  userId === userParamsId)
 }
--- packages/ht-core/src/user/getUser.js
+++ packages/ht-core/src/user/getUser.js
@@ -1,13 +1,13 @@
-import {User} from '../models'
+import { User } from '../models'
 
 const HIDDEN_USER_FIELDS = ['gender', 'locationInfo', 'birthday']
 
-export default function getUser(query, options = false) {
-  const searchParams = HIDDEN_USER_FIELDS.reduce((acc, ele) => {
-    return acc + (options ? '+' : '-') + ele + ' '
-  }, '')
+export default function getUser(query, shouldDisplay = false) {
+  const searchParams = HIDDEN_USER_FIELDS.map(
+    param => `${shouldDisplay ? '+' : '-'}${param}`,
+  ).join(' ')
   return User.findOne(query)
-             .select(searchParams.slice(0, searchParams.length-1))
-             .populate('profile.avatar')
-             .populate('profile.cover')
+    .select(searchParams.slice(0, searchParams.length - 1))
+    .populate('profile.avatar')
+    .populate('profile.cover')
 }

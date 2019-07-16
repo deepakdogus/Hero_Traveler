8c6a5925a8695dcf29acb2588b98fd02b8564f13
DanielChen1234 <dchen8492@yahoo.com>
2019-07-16 15:47:26 -0400

    changed algo

--- packages/ht-core/src/user/getUser.js
+++ packages/ht-core/src/user/getUser.js
@@ -3,9 +3,9 @@ import { User } from '../models'
 const HIDDEN_USER_FIELDS = ['gender', 'locationInfo', 'birthday']
 
 export default function getUser(query, shouldDisplay = false) {
-  const searchParams = HIDDEN_USER_FIELDS.map(
-    param => `${shouldDisplay ? '+' : '-'}${param}`,
-  ).join(' ')
+  const searchParams = HIDDEN_USER_FIELDS.reduce((acc, params) => {
+    return acc + (shouldDisplay ? '+' : '-') + params + ' '
+  }, '')
   return User.findOne(query)
     .select(searchParams.slice(0, searchParams.length - 1))
     .populate('profile.avatar')

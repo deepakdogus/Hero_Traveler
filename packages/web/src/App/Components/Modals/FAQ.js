23e85543c822c55173673d7aa93c992a9303a931
Dima <dparkhomenko@ksf.kiev.ua>
2019-07-25 20:06:13 +0300

    Updated FAQ modal.

--- packages/web/src/App/Components/Modals/FAQ.js
+++ packages/web/src/App/Components/Modals/FAQ.js
@@ -24,13 +24,13 @@ const Answer = styled.p`
 
 const FAQs = [
 {q: 'What is HERO Traveler?', a: 'HERO Traveler is a first-of-its-kind platform that is all about coming together to share life’s incredible adventures.'},
-{q: 'How do I join HERO Traveler?', a: 'Signing up is free and painless. You can either sign up within the app by creating a unique username, or quickly sign up by tapping the “Sign Up with Facebook” or “Sign up with Twitter” buttons on the Sign Up page.'},
-{q: 'How do I post a story?', a: 'On the HERO Traveler App, simply tap the “plus” sign at the bottom of any page to bring up the option to create a story or start filming a video.'},
+{q: 'How do I join HERO Traveler?', a: 'Signing up is free and painless. You can either sign up within the app by creating a unique username, or quickly sign up by tapping the “Sign Up with Facebook” button on the Sign Up page.'},
+{q: 'How do I post a story?', a: 'On the HERO Traveler site, simply click on the “Create” button at the top part of any page to bring upon the create a story page.'},
 {q: 'Can I just post photos without any captions?', a: 'Of course! When creating a story, all you need is a cover photo and a title. However you want to curate your adventure is up to you!'},
 {q: 'Do I need to shoot videos within the HERO Traveler app?', a: 'Nope, feel free to import videos. But remember that HERO Traveler is based around vertical video content. So unless you want to end up like Greedo at the cantina, you better shoot straight.'},
 {q: 'How do I import a video?', a: 'After selecting the video option, simply tap at the bottom of the screen on “library” to bring up your videos from your camera roll.'},
 {q: 'Does HERO Traveler support 4K Video?', a: 'While you cannot take 4K videos within the app itself just yet, you can still import 4K videos.'},
-{q: 'How do I become a HERO Traveler Contributor?', a: 'On the HERO Traveler App, simply tap the “plus” sign at the bottom of any page to bring up the option to create a story or start filming a video.'},
+{q: 'How do I become a HERO Traveler Contributor?', a: 'Contributors are the cream of the crop, representing our most passionate users. To be labeled with the “Contributor” badge on your profile, you must first publish 200 stories or more. To learn about other eligibility requirements send us an email at <a href="mailto:contributors@herotraveler.com">contributors@herotraveler.com</a>'},
 ]
 
 export default class FAQ extends Component {
@@ -39,7 +39,7 @@ export default class FAQ extends Component {
       return (
         <Container key={index}>
           <Question>{FAQ.q}</Question>
-          <Answer>{FAQ.a}</Answer>
+          <Answer dangerouslySetInnerHTML={{__html: FAQ.a}} />
         </Container>
       )
     })
@@ -49,8 +49,6 @@ export default class FAQ extends Component {
     return (
       <Wrapper>
         {this.renderFAQs(FAQs)}
-        <Question>How do I post a story?</Question>
-        <Answer>Contributors are the cream of the crop, representing our most passionate users. To be labeled with the “Contributor” badge on your profile, you must first publish 200 stories or more. To learn about other eligibility requirements send us an email at <a href="mailto:contributors@herotraveler.com">contributors@herotraveler.com</a></Answer>
       </Wrapper>
     )
   }
